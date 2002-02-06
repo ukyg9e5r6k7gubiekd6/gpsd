04dcd04319abc89c37ff595a0f688d581685461f
Remco Treffkorn <remco@rvt.com>
2002-02-06 00:59:14 +0000

    *** empty log message ***

--- gpsd.c
+++ gpsd.c
@@ -35,7 +35,6 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/time.h>
 #include <netinet/in.h>
 #include <stdio.h>
 
--- netlib.c
+++ netlib.c
@@ -8,7 +8,6 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/time.h>
 #include <netinet/in.h>
 #include <varargs.h>
 #include <netdb.h>
