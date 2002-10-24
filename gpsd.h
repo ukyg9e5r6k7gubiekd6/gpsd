56339640107c598069dd5fefd16a00f010572c9e
Remco Treffkorn <remco@rvt.com>
2002-10-24 21:49:06 +0000

    added prototype for send_nmea

--- gpsd.h
+++ gpsd.h
@@ -2,6 +2,7 @@
 
 enum { DEVICE_GENERIC, DEVICE_TRIPMATE, DEVICE_EARTHMATE, DEVICE_EARTHMATEb };
 
+extern void send_nmea(fd_set *afds, fd_set *nmea_fds, char *buf);
 extern int serial_open();
 extern void serial_close();
 extern void handle_message(char *sentence);
