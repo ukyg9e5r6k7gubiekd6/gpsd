059e1b9b5f05b1c09b2281ba19d6c4e92c4280e3
Eric S. Raymond <esr@thyrsus.com>
2004-08-17 22:46:09 +0000

    More things are internals now.

--- gpsd.h
+++ gpsd.h
@@ -67,23 +67,21 @@ extern int gps_process_NMEA_message(char *sentence, struct gps_data *outdata);
 extern void gps_NMEA_handle_message(struct gpsd_t *session, char *sentence);
 extern void gps_add_checksum(char *sentence);
 extern short gps_checksum(char *sentence);
-
-/* GPS library supplies these */
-int gps_open(char *device_name, int device_speed);
-void gps_close();
-int netlib_passiveTCP(char *service, int qlen);
-int netlib_connectTCP(char *host, char *service);
-int netlib_connectsock(char *host, char *service, char *protocol);
+extern int gps_open(char *device_name, int device_speed);
+extern void gps_close();
+extern int netlib_passiveTCP(char *service, int qlen);
+extern int netlib_connectTCP(char *host, char *service);
+extern int netlib_connectsock(char *host, char *service, char *protocol);
 
 /* High-level interface */
-void gps_init(struct gpsd_t *session, 
+extern void gps_init(struct gpsd_t *session, 
 	      int timeout, char devtype, char *dgpsserver);
-int gps_activate(struct gpsd_t *session);
-void gps_deactivate(struct gpsd_t *session);
-void gps_poll(struct gpsd_t *session);
-void gps_wrap(struct gpsd_t *session);
+extern int gps_activate(struct gpsd_t *session);
+extern void gps_deactivate(struct gpsd_t *session);
+extern void gps_poll(struct gpsd_t *session);
+extern void gps_wrap(struct gpsd_t *session);
 
 /* caller must supply this */
-void gpscli_report(int d, const char *fmt, ...);
+extern void gpscli_report(int d, const char *fmt, ...);
 
 
