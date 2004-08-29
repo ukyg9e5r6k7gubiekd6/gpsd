68252de0e0ecb771519b02982405f5f0076c28e3
Eric S. Raymond <esr@thyrsus.com>
2004-08-29 19:24:39 +0000

    Better explanatory comments. All members in this structure are now actually filled by libgps.a.

--- gps.h
+++ gps.h
@@ -17,6 +17,7 @@ struct life_t
 
 struct gps_data_t {
     int	online;			/* 1 if GPS is on line, 0 if not.
+				 *
 				 * Note: gpsd clears this flag when sentences
 				 * fail to show up within the GPS's normal
 				 * send cycle time. If the host-to-GPS 
@@ -25,24 +26,29 @@ struct gps_data_t {
 				 * prone to false negatives.
 				 */
     struct life_t online_stamp;
-    char utc[32];		/* UTC date/time as "mm/dd/yy hh:mm:ss.ssss".
-				 * Gets updated on every fix (GGA, GLL, or
-				 * GPRMC). The hhmmss.ss part is reliable.
-				 * The century part of the year is spliced
-				 * in from host-machine time and could be
-				 * wrong if the host and GPS are on opposite
-				 * sides of the International Date Line and
-				 * it's close to midnight of a new century.
-				 * Within one GPS send cycle after any 
-				 * midnight, if the last sentence was GGA or
-				 * GLL and not GPRMC, the date could be off
-				 * by one.  Altitude could be one send cycle
-				 * older than the timestamp if the last 
+    char utc[28];		/* UTC date/time as "yyy-mm-ddThh:mm:ss.sssZ".
+				 *
+				 * Updated on every fix (GGA, GLL or
+				 * GPRMC). The hhmmss.ss part is reliable to
+				 * within one GPS send cycle time (normally one
+				 * second).  Altitude could be one send cycle
+				 * older than the timestamp if the last
 				 * sentence was GPRMC.
+				 * 
+				 * Within one GPS send cycle after any
+				 * midnight, if the last sentence was GGA or
+				 * GLL and not GPRMC, the date could be off by
+				 * one.
+				 *
+				 * The century part of the year is spliced in
+				 * from host-machine time and could be wrong if
+				 * the host and GPS are on opposite sides of
+				 * the International Date Line and it's close
+				 * to midnight of a new century. 
 				 */
     /* location */
-    double latitude;		/* Latitude */
-    double longitude;		/* Longitude */
+    double latitude;		/* Latitude in degrees */
+    double longitude;		/* Longitude in degrees */
     struct life_t latlon_stamp;
     double altitude;		/* Altitude in meters */
     struct life_t altitude_stamp;
@@ -50,17 +56,16 @@ struct gps_data_t {
     /* velocity */
     double speed;		/* Speed over ground, knots */
     struct life_t speed_stamp;
-    double track;		/* Course made good */
+    double track;		/* Course made good (relative to true north) */
     struct life_t track_stamp;
-    double mag_var;		/* Magnetic variation in degrees */
 
     /* status of fix */
-    int    status;		/* do we have a fix? */
+    int    status;		/* Do we have a fix? */
 #define STATUS_NO_FIX	0	/* no */
 #define STATUS_FIX	1	/* yes, without DGPS */
 #define STATUS_DGPS_FIX	2	/* yes, with DGPS */
     struct life_t status_stamp;
-    int    mode;		/* mode of fix */
+    int    mode;		/* Mode of fix */
 #define MODE_NO_FIX	1	/* none */
 #define MODE_2D  	2	/* good for latitude/longitude */
 #define MODE_3D  	3	/* good for altitude too */
@@ -68,11 +73,11 @@ struct gps_data_t {
 
     /* precision of fix */
     int satellites_used;	/* Number of satellites used in solution */
-    int used[MAXCHANNELS];	/* used in last fix? */
+    int used[MAXCHANNELS];	/* Used in last fix? */
     double pdop;		/* Position dilution of precision, meters */
     double hdop;		/* Horizontal dilution of precision, meters */
     double vdop;		/* Vertical dilution of precision, meters */
-    double separation;		/* geoidal separation */
+    double separation;		/* Geoidal separation [2] */
     struct life_t fix_quality_stamp;
 
     /* satellite status */
@@ -94,14 +99,6 @@ struct gps_data_t {
     int Zv[MAXCHANNELS];	/* signal values (0-7) */
     struct life_t signal_quality_stamp;
 #endif /* PROCESS_PRWIZCH */
-
-    /* stuff after this point is private */
-    int year;
-    int month;
-    int day;
-    int hours;
-    int minutes;
-    int seconds;
 
     void (*raw_hook)(char *buf);	/* raw-mode hook for GPS data */
 };
