47bfd0b80f7f9c3f01664471f701bdcc4ca15527
Eric S. Raymond <esr@thyrsus.com>
2004-09-29 16:20:51 +0000

    Dependency cleanup.

--- serial.c
+++ serial.c
@@ -1,10 +1,6 @@
 #include "config.h"
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <fcntl.h>
 
 #if defined (HAVE_SYS_TERMIOS_H)
