c55bf6d5b7c86e9f969358da7c3434d8ac68dbce
Eric S. Raymond <esr@thyrsus.com>
2005-02-14 00:30:54 +0000

    We seem to have found the minimum settle time.

--- sirf.c
+++ sirf.c
@@ -23,7 +23,7 @@ int sirf_mode(struct gps_session_t *session, int binary, int speed)
    int status = nmea_send(session->gNMEAdata.gps_fd, 
 		    "$PSRF100,%d,%d,8,1,0", !binary, speed);
    gpsd_report(1, "Send returned %d.\n", status);
-   sleep(1);
+   usleep(50000);	/* definitely fails below 40 milliseconds */
    return status && 
 	gpsd_set_speed(session->gNMEAdata.gps_fd, &session->ttyset, (speed_t)speed);
 }
