d7b50e9192bbc308a2f59bcf4e195003e64979b8
Eric S. Raymond <esr@thyrsus.com>
2005-03-16 21:24:35 +0000

    Force an initializer call.

--- libgpsd_core.c
+++ libgpsd_core.c
@@ -101,6 +101,8 @@ int gpsd_activate(struct gps_session_t *session)
 	    gpsd_switch_driver(session, "SiRF-II binary");
 	else if (session->packet_type == NMEA_PACKET)
 	    gpsd_switch_driver(session, "Generic NMEA");
+	else if (session->device_type->initializer)
+	    session->device_type->initializer(session);
 
 	return session->gNMEAdata.gps_fd;
     }
