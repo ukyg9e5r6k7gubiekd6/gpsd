d25f19f76bd7f867d6c4c75d9bf3fff729cdc4df
Eric S. Raymond <esr@thyrsus.com>
2005-05-26 05:17:43 +0000

    Re-apply the fix for Gary.

--- serial.c
+++ serial.c
@@ -109,6 +109,8 @@ int gpsd_open(struct gps_device_t *session)
 	    if ((*dp)->probe && (*dp)->probe(session)) {
 		gpsd_report(3, "probe found %s driver...\n", (*dp)->typename);
 		session->device_type = *dp;
+		if (session->device_type->initializer)
+		    session->device_type->initializer(session);
 		return session->gpsdata.gps_fd;
 	    }
  	}
