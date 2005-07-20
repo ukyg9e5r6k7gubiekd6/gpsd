c665febb750bd0064e4596ade24af984f48377f6
Eric S. Raymond <esr@thyrsus.com>
2005-07-20 13:32:15 +0000

    We can now see byte offsets when checking a decode.

--- rtcmdecode.c
+++ rtcmdecode.c
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 #include "gpsd.h"
 
@@ -29,10 +31,16 @@ int main(int argc, char **argv)
     int             c;
     struct gps_device_t device;
     enum rtcmstat_t res;
+    off_t count;
     char buf[BUFSIZ];
+    bool striplines = false;
 
-    while ((c = getopt(argc, argv, "v:")) != EOF) {
+    while ((c = getopt(argc, argv, "bv:")) != EOF) {
 	switch (c) {
+	case 'b':
+	    striplines = true;
+	    break;
+
 	case 'v':		/* verbose */
 	    verbose = RTCM_ERRLEVEL_BASE + atoi(optarg);
 	    break;
@@ -46,10 +54,21 @@ int main(int argc, char **argv)
     argc -= optind;
     argv += optind;
 
+    /* strip lines with leading # */
+    if (striplines) {
+	while ((c = getchar()) == '#')
+	    (void)fgets(buf, (int)sizeof(buf), stdin);
+	(void)ungetc(c, stdin);
+    }
+
     rtcm_init(&device);
 
+    count = 0;
     while ((c = getchar()) != EOF) {
 	res = rtcm_decode(&device, (unsigned int)c);
+	if (verbose >= RTCM_ERRLEVEL_BASE + 3) 
+	    printf("%08lu: '%c' [%02x] -> %d\n", 
+		   (unsigned long)count++, (isprint(c)?c:'.'), (unsigned)(c & 0xff), res);
 	if (res == RTCM_STRUCTURE) {
 	    rtcm_dump(&device, buf, sizeof(buf));
 	    (void)fputs(buf, stdout);
