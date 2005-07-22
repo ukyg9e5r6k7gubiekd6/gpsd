2d90f82877e6d180e1debd4472bbac3083ea47ad
Eric S. Raymond <esr@thyrsus.com>
2005-07-22 13:30:04 +0000

    Add ability to list macros.
    Sort IDs longest first to avoid false prefix matches.  Still not
    idempotent.  Still clobbers rangerate.

--- wrapinvert.py
+++ wrapinvert.py
@@ -2,12 +2,18 @@
 #
 # Wrap bitfield references in rtcm.c in inversion macros.
 
-import sys, re
+import sys, re, getopt
 
 state = 0
 ids = {}
 
-refmatch = "[a-z]*->w[0-9]+\.%s"	# Match a bitfield reference
+refmatch = "[a-z_]*->w[0-9]+\.%s"	# Match a bitfield reference
+
+(options, arguments) = getopt.getopt(sys.argv[1:], "l")
+list = False;
+for (switch, val) in options:
+    if (switch == '-l'):
+        list = True
 
 while True:
     line = sys.stdin.readline()
@@ -21,6 +27,10 @@ while True:
         state = 0
     elif line.find("static void unpack") > -1:
         state = 2
+        # Sort keys so longest IDs will be matched longest first
+        keys = ids.keys()
+        keys.sort(lambda x, y: len(y) - len(x) or cmp(x, y))
+        sys.stderr.write(`keys` + "\n")
     elif state == 2 and line[0] == '}':
         state = 0
 
@@ -47,13 +57,25 @@ while True:
 
     # Some lines get copied out unaltered, some get hacked.
     if state <= 1:
-        #sys.stdout.write(line)
+        if not list:
+            sys.stdout.write(line)
         continue
 
     # Do the actual substitutions
-    for (key, value) in ids.items():
+    for key in keys:
         m = re.compile(refmatch % key).search(line)
         if m:
-            line = line[:m.start(0)] + value + "(" + m.group(0) + ")" + line[m.end(0):]
-    sys.stdout.write(line)
-        
+            line = line[:m.start(0)] + ids[key] + "(" + m.group(0) + ")" + line[m.end(0):]
+    if not list:
+        sys.stdout.write(line)
+
+if list:
+    already = []
+    vals = ids.values()
+    vals.sort()
+    print "#if __BYTE_ORDER == __LITTLE_ENDIAN"
+    for v in vals:
+        if not v in already:
+            print "#define %s(x)	x" % v
+            already.append(v)
+    print "#endif"
