c1533724585d3c0ea03c538aaca9c2fe953274aa
Eric S. Raymond <esr@thyrsus.com>
2005-07-24 15:08:14 +0000

    This change might keep us from losing on a 16-bit machine.

--- gps.h
+++ gps.h
@@ -89,8 +89,12 @@ struct gps_fix_t {
 /* RTCM104 doesn't specify this, so give it the largest reasonable value */
 #define MAXHEALTH	(RTCM_WORDS_MAX-2)	
 
-/* nominally 30-bit word */
-typedef /*@unsignedintegraltype@*/ unsigned int isgps30bits_t;
+/*
+ * A nominally 30-bit word (24 bits of data, 6 bits of parity)
+ * used both in the GPS downlink protocol described in IS-GPS-200
+ * and in the format for DGPS corrections used in RTCM-104.
+ */
+typedef /*@unsignedintegraltype@*/ u_int32_t isgps30bits_t;
 
 struct rtcm_t {
     /* header contents */
