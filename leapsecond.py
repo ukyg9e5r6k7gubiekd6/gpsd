981cdc4925753ca62a3a0bdd2bf0d4a17871a66d
Eric S. Raymond <esr@thyrsus.com>
2006-06-08 00:04:14 +0000

    Remote retrieval works.

--- leapsecond.py
+++ leapsecond.py
@@ -4,23 +4,38 @@
 # GPS time, which changes occasionally due to variations in the Earth's
 # rotation.
 #
-import urllib, re
+import urllib, re, random
 
-__locations = (
+__locations = [
     (
+    # U.S. Navy's offset-history file
+    "ftp://maia.usno.navy.mil/ser7/tai-utc.dat",
+    r" TAI-UTC= +([0-9-]+)[^\n]*\n$",
+    -1,
+    ),
+    (
+    # International Earth Rotation Service Bulletin C
     "http://hpiers.obspm.fr/iers/bul/bulc/bulletinc.dat",
     r" UTC-TAI = ([0-9-]+)",
+    1,
     ),
-)
+]
 
-def get():
+def retrieve():
     "Retrieve current leap-second from web sources."
-    for (url, regexp) in __locations:
-        ifp = urllib.urlopen(url)
-        txt = ifp.read()
-        ifp.close()
-        m = re.search(regexp, txt)
-        return int(m.group(1))
-    
+    random.shuffle(__locations)	# To spread the load
+    for (url, regexp, sign) in __locations:
+        try:
+            ifp = urllib.urlopen(url)
+            txt = ifp.read()
+            ifp.close()
+            m = re.search(regexp, txt)
+            if m:
+                return int(m.group(1)) * sign
+        except:
+            pass
+    else:
+        raise ValueError
+
 if __name__ == '__main__':
-    print "GPS offset is: %d\n" % get()
+    print "GPS offset is: %d" % retrieve()
