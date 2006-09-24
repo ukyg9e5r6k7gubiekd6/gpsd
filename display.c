c9eb8474eb7c39ab2b9a10a0762d0aa9b691ceb4
Chris Kuethe <chris.kuethe@gmail.com>
2006-09-24 00:12:34 +0000

    Set the PRN tag to grey text if it wasn't used in the position solution.

--- display.c
+++ display.c
@@ -129,7 +129,10 @@ void draw_graphics(struct gps_data_t *gpsdata)
 		     0, 360 * 64	/* angle1, angle2 */
 		);
 	    (void)snprintf(buf, sizeof(buf), "%-3d", gpsdata->PRN[i]);
-	    set_color("Blue");
+	    if(gpsdata->used[i])
+	        set_color("Blue");
+	    else
+		set_color("Grey");
 	    (void)XDrawString(XtDisplay(draww), pixmap, drawGC, x, y + 17, buf, 3);
 	    if (gpsdata->ss[i]) {
 		set_color("Black");
