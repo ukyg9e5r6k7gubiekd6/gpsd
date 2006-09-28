8041ccf4917dad4da3b9a4f827084a6785049960
Chris Kuethe <chris.kuethe@gmail.com>
2006-09-28 19:52:21 +0000

    Make the hexdump-to-binary converter part of libgpsd

--- libgpsd_core.c
+++ libgpsd_core.c
@@ -682,3 +682,45 @@ char /*@ observer @*/ *gpsd_hexdump(const void *binbuf, size_t binbuflen)
     return hexbuf;
 }
 
+int gpsd_hexpack(char *src, char *dst, int len){
+    int i, k, l;
+
+    l = strlen(src) / 2;
+    if ((l < 1) || (l > len))
+	return -1;
+
+    bzero(dst, len);
+    for (i = 0; i < l; i++)
+	if ((k = hex2bin(src+i*2)) != -1)
+	    dst[i] = (char)(k & 0xff);
+	else
+	    return -1;
+    return l;
+}
+
+int hex2bin(char *s){
+    int a, b;
+
+    a = s[0] & 0xff;
+    b = s[1] & 0xff;
+
+    if ((a >= 'a') && (a <= 'z'))
+	a = a + 10 - 'a';
+    else if ((a >= 'A') && (a <= 'Z'))
+	a = a + 10 - 'A';
+    else if ((a >= '0') && (a <= '9'))
+	a -= '0';
+    else
+	return -1;
+
+    if ((b >= 'a') && (b <= 'z'))
+	b = b + 10 - 'a';
+    else if ((b >= 'A') && (b <= 'Z'))
+	b = b + 10 - 'A';
+    else if ((b >= '0') && (b <= '9'))
+	b -= '0';
+    else
+	return -1;
+
+    return ((a<<4) + b);
+}
