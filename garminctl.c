05d846bf25f8975be9a3f9a45fa6f1d311d6c66a
Chris Kuethe <chris.kuethe@gmail.com>
2006-10-26 23:13:57 +0000

    libgps provides strlcat on systems where it is not provided by libc...
    ...and gpsd_config.h provides the prototype.

--- garminctl.c
+++ garminctl.c
@@ -34,9 +34,6 @@ static int debug_level = 0;
 static void logit(int , char *, ...);
 static void nmea_add_checksum(char *);
 static int nmea_send(int , const char *, ... );
-#ifndef HAVE_STRLCAT
-size_t strlcat(char *dst, const char *src, size_t size);
-#endif
 
 /* how many characters to look at when trying to find baud rate lock */
 #define SNIFF_RETRIES	1200
@@ -51,65 +48,6 @@ void logit( int level, char *fmt, ...)  {
     (void)vprintf(fmt, ap);
     va_end(ap);
 }
-
-
-#ifndef HAVE_STRLCAT
-/*	$OpenBSD: strlcat.c,v 1.13 2005/08/08 08:05:37 espie Exp $	*/
-
-/*
- * Copyright (c) 1998 Todd C. Miller <Todd.Miller@courtesan.com>
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
-
-/*
- * Appends src to string dst of size siz (unlike strncat, siz is the
- * full size of dst, not space left).  At most siz-1 characters
- * will be copied.  Always NUL terminates (unless siz <= strlen(dst)).
- * Returns strlen(src) + MIN(siz, strlen(initial dst)).
- * If retval >= siz, truncation occurred.
- */
-/*@ -usedef -mustdefine @*/
-size_t
-strlcat(char *dst, const char *src, size_t siz)
-{
-	char *d = dst;
-	const char *s = src;
-	size_t n = siz;
-	size_t dlen;
-
-	/* Find the end of dst and adjust bytes left but don't go past end */
-	while (n-- != 0 && *d != '\0')
-		d++;
-	dlen = (size_t)(d - dst);
-	n = siz - dlen;
-
-	if (n == 0)
-		return(dlen + strlen(s));
-	while (*s != '\0') {
-		if (n != 1) {
-			*d++ = *s;
-			n--;
-		}
-		s++;
-	}
-	*d = '\0';
-
-	return(dlen + (s - src));	/* count does not include NUL */
-}
-/*@ +usedef +mustdefine @*/
-#endif /* HAVE_STRLCAT */
-
 
 /*****************************************************************************
  *
