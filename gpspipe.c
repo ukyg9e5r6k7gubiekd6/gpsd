7ceed6ecff8a55d9656e8dc3390f2997a05b8265
Eric S. Raymond <esr@thyrsus.com>
2006-10-31 06:56:39 +0000

    Get rid of several flag variables...
    ...these aren't necessary when we can assemble and test a buffer of
    GPSD commands directly.

--- gpspipe.c
+++ gpspipe.c
@@ -82,29 +82,24 @@ static void usage(void)
 {
     (void)fprintf(stderr, "Usage: gpspipe [OPTIONS] [server[:port]]\n\n"
 		  "SVN ID: $Id$ \n"
-		  "-h show this help\n"
-		  "-r Dump raw NMEA\n"
-		  "-R Dump super-raw mode (gps binary)\n"
-		  "-w Dump gpsd native data\n"
-		  "-j turn on server-side buffering\n"
-		  "-t time stamp the data\n"
-		  "-s [serial dev] emulate a 4800bps NMEA GPS on serial port (use with '-r')\n"
-		  "-n [count] exit after count packets\n"
-		  "-V print version and exit\n\n"
-		  "You must specify one, or both, of -r/-w\n"
+		  "-h Show this help.\n"
+		  "-r Dump raw NMEA.\n"
+		  "-R Dump super-raw mode (GPS binary).\n"
+		  "-w Dump gpsd native data.\n"
+		  "-j Turn on server-side buffering.\n"
+		  "-t Time stamp the data.\n"
+		  "-s [serial dev] emulate a 4800bps NMEA GPS on serial port (use with '-r').\n"
+		  "-n [count] exit after count packets.\n"
+		  "-V Print version and exit.\n\n"
+		  "You must specify one, or both, of -r/-w.\n"
 	);
 }
 
 int main( int argc, char **argv) 
 {
-    int s = 0;
+    int sock = 0;
     char buf[4096];
-    char *cstr = NULL;
     ssize_t wrote = 0;
-    bool dump_super_raw = false;
-    bool dump_nmea = false;
-    bool dump_gpsd = false;
-    bool jitter_flag = false;
     bool timestamp = false;
     bool new_line = true;
     long count = -1;
@@ -113,25 +108,26 @@ int main( int argc, char **argv)
     char *port = DEFAULT_GPSD_PORT, *server = "127.0.0.1";
     char *serialport = NULL;
 
+    buf[0] = '\0';
     while ((option = getopt(argc, argv, "?hrRwjtVn:s:")) != -1) {
 	switch (option) {
 	case 'n':
 	    count = strtol(optarg, 0, 0);
 	    break;
 	case 'r':
-	    dump_nmea = true;
+	    strlcat(buf, "r=1;", sizeof(buf));
 	    break;
 	case 'R':
-	    dump_super_raw = true;
+	    strlcat(buf, "r=2;", sizeof(buf));
 	    break;
 	case 't':
 	    timestamp = true;
 	    break;
 	case 'w':
-	    dump_gpsd = true;
+	    strlcat(buf, "w=1;", sizeof(buf));
 	    break;
 	case 'j':
-	    jitter_flag = true;
+	    strlcat(buf, "j=1;", sizeof(buf));
 	    break;
 	case 'V':
 	    (void)fprintf(stderr, "%s: SVN ID: $Id$ \n", argv[0]);
@@ -147,26 +143,13 @@ int main( int argc, char **argv)
 	}
     }
 
-    if (serialport != NULL && !dump_nmea) {
-	fprintf(stderr,"Use of '-s' requires '-r'\n");
+    if (serialport!=NULL && strstr(buf, "r=1")==NULL) {
+	(void)fprintf(stderr, "gpsipipe: use of '-s' requires '-r'.\n");
 	exit(1);
     }
 
-    if (dump_super_raw) {
-	cstr = "R=2\n";
-	/* super raw overrides NMEA and GPSD modes */
-	dump_nmea = false;
-	dump_gpsd = false;
-    } else if (dump_nmea) {
-	if (dump_gpsd) {
-	    cstr = "rw\n";
-	} else {
-	    cstr = "r\n";
-	}
-    } else if (dump_gpsd) {
-	cstr = "w=1\n";
-    } else {
-	usage();
+    if (strstr(buf, "r=")==NULL && strstr(buf, "w=1")==NULL) {
+	(void)fprintf(stderr, "gpspipe: one of '-r' or '-w' is required.\n");
 	exit(1);
     }
     /* Grok the server, port, and device. */
@@ -201,26 +184,18 @@ int main( int argc, char **argv)
 	open_serial(serialport);
 
     /*@ -nullpass @*/
-    s = netlib_connectsock( server, port, "tcp");
-    if (s < 0) {
-	fprintf(stderr, "gpspipe: could not connect to gpsd %s:%s, %s(%d)\n",
-		 server, port, strerror(errno), errno);
+    sock = netlib_connectsock( server, port, "tcp");
+    if (sock < 0) {
+	(void)fprintf(stderr, 
+		      "gpspipe: could not connect to gpsd %s:%s, %s(%d)\n",
+		      server, port, strerror(errno), errno);
 	exit(1);
     }
     /*@ +nullpass @*/
 
-    if (jitter_flag) {
-	char *jstr = "j=1\n";
-	wrote = write(s, jstr, strlen(jstr));
-	if ((ssize_t)strlen(jstr) != wrote) {
-	    fprintf(stderr, "gpspipe: write error, %s(%d)\n", 
-		    strerror(errno), errno);
-	    exit(1);
-	}
-    }
-
-    wrote = write(s, cstr, strlen(cstr));
-    if ((ssize_t)strlen(cstr) != wrote) {
+    /* ship the assembled options */
+    wrote = write(sock, buf, strlen(buf));
+    if ((ssize_t)strlen(buf) != wrote) {
 	(void)fprintf(stderr, "gpspipe: write error, %s(%d)\n", 
 		      strerror(errno), errno);
 	exit(1);
@@ -231,7 +206,7 @@ int main( int argc, char **argv)
 	int j = 0;
 	int readbytes = 0;
 
-	readbytes = (int)read(s, buf, sizeof(buf));
+	readbytes = (int)read(sock, buf, sizeof(buf));
 	if (readbytes > 0) {
 	    for (i = 0 ; i < readbytes ; i++) {
 		char c = buf[i];
@@ -249,13 +224,13 @@ int main( int argc, char **argv)
 			exit(1);
 		    }
 		}
-		if (fputc( c, stdout) == EOF) {
+		if (fputc(c, stdout) == EOF) {
 		    fprintf( stderr, "gpspipe: Write Error, %s(%d)\n",
 			     strerror(errno), errno);
 		    exit(1);
 		}
 
-		if ( c == '\n' ) {
+		if (c == '\n') {
 		    if (serialport != NULL) {
 			if (write(fd_out, serbuf, (size_t)j) == -1) {
 			    fprintf(stderr, 
