ec68199659accfb8ef308066f0a5f0e3f4aac853
Chris Kuethe <chris.kuethe@gmail.com>
2006-11-07 22:35:20 +0000

    Updated cgps with support for device dependent behaviour.
    >From Jeff Francis, plus a couple of cleanups from me.

--- cgps.c
+++ cgps.c
@@ -40,7 +40,8 @@
 #include "gps.h"
 
 static struct gps_data_t *gpsdata;
-static time_t timer;    /* time of last state change */
+static time_t status_timer;    /* Time of last state change. */
+static time_t misc_timer;    /* Misc use timer. */
 static int state = 0;   /* or MODE_NO_FIX=1, MODE_2D=2, MODE_3D=3 */
 static float altfactor = METERS_TO_FEET;
 static float speedfactor = MPS_TO_MPH;
@@ -52,6 +53,11 @@ static WINDOW *datawin, *satellites, *messages, *command, *status;
 static int silent_flag=0;
 static int fixclear_flag=0;
 static int bigger;
+
+static int nosats_flag=0;
+static int compass_flag=0;
+static int got_gps_type=0;
+static char gps_type[20];
 
 /* Function to call when we're all done.  Does a bit of clean-up. */
 static void die(int sig UNUSED) 
@@ -79,8 +85,108 @@ static void die(int sig UNUSED)
 
 static enum deg_str_type deg_type = deg_dd;
 
-/* This gets called once for each new sentence. */
-static void update_panel(struct gps_data_t *gpsdata, 
+/* This gets called once for each new sentence until we figure out
+   what's going on and switch to either update_gps_panel() or
+   update_compass_panel(). */
+static void update_probe(struct gps_data_t *gpsdata, 
+		       char *message,
+		       size_t len UNUSED , 
+		       int level UNUSED)
+{
+  /* Send an 'i' once per second until we figure out what the GPS
+     device is. */
+  if(time(NULL)-misc_timer > 2) {
+    (void)gps_query(gpsdata, "i\n");
+    (void)fprintf(stderr,"Probing...\n");
+    misc_timer=time(NULL);
+  }
+
+  if(strncmp(message,"GPSD,I=",6)==0) {
+    message+=7;
+    (void)strlcpy(gps_type, message, 20);
+    got_gps_type=1;
+
+    /* We make some screen layout changes depending on what we find.
+       Garmin binary does not give us satellite data, for example, so
+       we won't display a satellite window if we detect that. */
+    if(strstr(message,"Garmin") && strstr(message,"binary")) {
+      nosats_flag=1;
+    }
+
+    /* If we're hooked to a compass, we display an entirely different
+       screen and label the data much differently. */
+    if(strstr(message,"True North")) {
+      compass_flag=1;
+    }
+  }
+}
+
+
+/* This gets called once for each new compass sentence. */
+static void update_compass_panel(struct gps_data_t *gpsdata, 
+                         char *message,
+                         size_t len UNUSED , 
+                         int level UNUSED)
+{
+  char *s;
+
+  /* Print time/date. */
+  (void)wmove(datawin, 1,17);
+  if (isnan(gpsdata->fix.time)==0) {
+    char scr[128];
+    (void)wprintw(datawin,"%s",unix_to_iso8601(gpsdata->fix.time, scr, (int)sizeof(s)));
+  } else
+    (void)wprintw(datawin,"n/a                    ");
+
+  /* Fill in the heading. */
+  (void)wmove(datawin, 2,17);
+  if (isnan(gpsdata->fix.track)==0) {
+    (void)wprintw(datawin,"%.1f     ", gpsdata->fix.track);
+  } else
+    (void)wprintw(datawin,"n/a         ");
+
+  /* Fill in the pitch. */
+  (void)wmove(datawin, 3,17);
+  if (isnan(gpsdata->fix.climb)==0) {
+    (void)wprintw(datawin,"%.1f     ", gpsdata->fix.climb);
+  } else
+    (void)wprintw(datawin,"n/a         ");
+
+  /* Fill in the roll. */
+  (void)wmove(datawin, 4,17);
+  if (isnan(gpsdata->fix.speed)==0)
+    (void)wprintw(datawin,"%.1f     ",gpsdata->fix.speed);
+  else
+    (void)wprintw(datawin,"n/a         ");
+
+  /* Fill in the speed. */
+  (void)wmove(datawin, 5,17);
+  if (isnan(gpsdata->fix.altitude)==0)
+    (void)wprintw(datawin,"%.1f     ", gpsdata->fix.altitude);
+  else
+    (void)wprintw(datawin,"n/a         ");
+
+  /* Fill in GPS type. */
+  (void)wmove(datawin, 6,17);
+  if(got_gps_type==1) {
+    (void)wprintw(datawin,"%s",gps_type);
+  } else {
+    (void)wprintw(datawin,"unknown     ");
+  }
+
+  /* Be quiet if the user requests silence. */
+  if(silent_flag==0) {
+    (void)wprintw(messages, "%s\n", message);
+  }
+
+  (void)wrefresh(datawin);
+  (void)wrefresh(messages);
+  (void)wrefresh(command);
+}
+
+
+/* This gets called once for each new GPS sentence. */
+static void update_gps_panel(struct gps_data_t *gpsdata, 
                          char *message,
                          size_t len UNUSED , 
                          int level UNUSED)
@@ -92,20 +198,20 @@ static void update_panel(struct gps_data_t *gpsdata,
   /* This is for the satellite status display.  Lifted almost
      verbatim from xgps.c.  Note that the satellite list may be
      truncated based on available screen size.  */
-  if (gpsdata->satellites) {
+  if (gpsdata->satellites && nosats_flag==0) {
     for (i = 0; i < (bigger + 12); i++) {
       (void)wmove(satellites, i+2, 1);
       if (i < gpsdata->satellites) {
-        (void)wprintw(satellites," %3d    %02d    %03d    %02d      %c  ",
-                      gpsdata->PRN[i],
-                      gpsdata->elevation[i], gpsdata->azimuth[i], 
-                      gpsdata->ss[i],   gpsdata->used[i] ? 'Y' : 'N');
+	(void)wprintw(satellites," %3d    %02d    %03d    %02d      %c  ",
+		      gpsdata->PRN[i],
+		      gpsdata->elevation[i], gpsdata->azimuth[i], 
+		      gpsdata->ss[i],   gpsdata->used[i] ? 'Y' : 'N');
       } else {
-        (void)wprintw(satellites,"                              ");
+	(void)wprintw(satellites,"                              ");
       }
     }
   }
-  
+
   /* Print time/date. */
   (void)wmove(datawin, 1,17);
   if (isnan(gpsdata->fix.time)==0) {
@@ -137,7 +243,7 @@ static void update_panel(struct gps_data_t *gpsdata,
   else
     (void)wprintw(datawin,"n/a         ");
 
-  /* Fill in the speed */
+  /* Fill in the speed. */
   (void)wmove(datawin, 5,17);
   if (gpsdata->fix.mode >= MODE_2D && isnan(gpsdata->fix.track)==0)
     (void)wprintw(datawin,"%.1f %s     ", gpsdata->fix.speed*speedfactor, speedunits);
@@ -187,6 +293,14 @@ static void update_panel(struct gps_data_t *gpsdata,
   else
     (void)wprintw(datawin,"n/a            ");
 
+  /* Fill in GPS type. */
+  (void)wmove(datawin, 8,17);
+  if(got_gps_type==1) {
+    (void)wprintw(datawin,"%s",gps_type);
+  } else {
+    (void)wprintw(datawin,"unknown     ");
+  }
+
   /* Fill in the GPS status and the time since the last state change. */
   (void)wmove(status, 1,10);
   if (gpsdata->online == 0) {
@@ -196,13 +310,13 @@ static void update_panel(struct gps_data_t *gpsdata,
     newstate = gpsdata->fix.mode;
     switch (gpsdata->fix.mode) {
     case MODE_2D:
-      (void)wprintw(status,"2D %sFIX (%d secs)   ",(gpsdata->status==STATUS_DGPS_FIX)?"DIFF ":"", (int) (time(NULL) - timer));
+      (void)wprintw(status,"2D %sFIX (%d secs)   ",(gpsdata->status==STATUS_DGPS_FIX)?"DIFF ":"", (int) (time(NULL) - status_timer));
       break;
     case MODE_3D:
-      (void)wprintw(status,"3D %sFIX (%d secs)   ",(gpsdata->status==STATUS_DGPS_FIX)?"DIFF ":"", (int) (time(NULL) - timer));
+      (void)wprintw(status,"3D %sFIX (%d secs)   ",(gpsdata->status==STATUS_DGPS_FIX)?"DIFF ":"", (int) (time(NULL) - status_timer));
       break;
     default:
-      (void)wprintw(status,"NO FIX (%d secs)     ", (int) (time(NULL) - timer));
+      (void)wprintw(status,"NO FIX (%d secs)     ", (int) (time(NULL) - status_timer));
       break;
     }
   }
@@ -212,15 +326,16 @@ static void update_panel(struct gps_data_t *gpsdata,
     (void)wprintw(messages, "%s\n", message);
   }
 
-  /* Reset the timer if the state has changed. */
+  /* Reset the status_timer if the state has changed. */
   if (newstate != state) {
-    timer = time(NULL);
+    status_timer = time(NULL);
     state = newstate;
   }
 
   (void)wrefresh(datawin);
   (void)wrefresh(status);
-  (void)wrefresh(satellites);
+  if(nosats_flag==0)
+    (void)wrefresh(satellites);
   (void)wrefresh(messages);
   (void)wrefresh(command);
 }
@@ -357,17 +472,44 @@ int main(int argc, char *argv[])
     exit(2);
   }
 
+  /* Set both timers to now. */
+  status_timer = time(NULL);
+  misc_timer = status_timer;
+
+  /* If the user requested a specific device, try to change to it. */
+  if (device)
+      (void)gps_query(gpsdata, "F=%s\n", device);
+
+  /* Here's where updates go until we figure out what we're dealing
+     with. */
+  gps_set_raw_hook(gpsdata, update_probe);
+
+  /* Tell me what you are... */
+  (void)gps_query(gpsdata, "i\n");
+
+  /* Loop for ten seconds looking for a device.  If none found, give
+     up and assume "unknown" device type. */
+  while(got_gps_type==0) {
+
+    /* Sleep for one second. */
+    timeout.tv_sec = 1;
+    timeout.tv_usec = 0;
+    select(0,NULL,NULL,NULL,&timeout);
+
+    /* Give up after ten seconds. */
+    if(time(NULL)-status_timer >= 10) {
+      (void)strlcpy(gps_type, "unknown", 20);
+      got_gps_type=1;
+    }
+  }
+
   /* If the user has requested the 'j' option (buffering), make the
      request of gpsd before we continue. */
-  if(fixclear_flag==1) {
+  if(fixclear_flag==1 && compass_flag==0) {
     (void)gps_query(gpsdata, "j=1\n");
   }
 
-  /* Update the timestamp (used to keep track of time since last state
-     change). */
-  timer = time(NULL);
-
-  /* Set up the curses screen (if using curses). */
+  /* Fire up curses. */
   (void)initscr();
   (void)noecho();
   (void)signal(SIGINT,die);
@@ -376,11 +518,16 @@ int main(int argc, char *argv[])
   /* See if the screen is big enough to show more than 12 sats. */
   (void)getmaxyx(stdscr,ysize,xsize);
 
-  /* If the user's screen is too small, exit gracefully.  At some
-     point, it might be reasonable to drop certain fields or even
-     entire windows to at least make the attempt to "do the right
-     thing" for small screens.  For now, we just bail.  We do run at
-     80x20, but we force the raw gpsd data scrolling flag off. */
+  /* If the user's screen is too small, exit gracefully.  We do run as
+     small as 80x20, but we force the raw gpsd data scrolling flag
+     off.
+
+     TODO: Allow sizes smaller than 80x20 by dropping less useful data
+     fields and showing only certain satellites (maybe the ones
+     actually used to compute the fix?)  
+
+     TODO: Allow smaller than 80x20 for a compass display, as there
+     are far fewer fields required. */
   if((ysize >= 20) && (ysize < 24) && (xsize >= 80)) {
     silent_flag=1;
   } else if((ysize < 24) || (xsize < 80)) {
@@ -392,61 +539,113 @@ int main(int argc, char *argv[])
     die(0);
   }
 
-  /* Grow the windows based on screen size, but not more than the
-     maximum allowed number of sats.  Always leave at least four
-     lines at the bottom of the screen to scroll gpsd raw data. */
-  bigger=ysize-24;
+  /* Set up the screen for either a compass or a gps receiver. */
+  if(compass_flag==1) {
+    /* We're a compass, set up accordingly. */
 
-  if(bigger<0) {
-    bigger=0;
+    /*@ -onlytrans @*/
+    datawin    = newwin(8, 45, 1, 0);
+    command    = newwin(3, 45, 9, 0);
+    messages   = newwin(0, 0, 12, 0);
+
+    /*@ +onlytrans @*/
+    (void)scrollok(messages, true);
+    (void)wsetscrreg(messages, 0, LINES-13);
+    (void)nodelay(messages,(bool)TRUE);
+    
+    (void)mvprintw(0, 31, "CGPS Test Client");
+
+    /*@ -nullpass @*/
+    (void)refresh();
+    /*@ +nullpass @*/
+    
+    /* Do the initial field label setup. */
+    (void)mvwprintw(datawin, 1,5, "Time:");
+    (void)mvwprintw(datawin, 2,5, "Heading:");
+    (void)mvwprintw(datawin, 3,5, "Pitch:");
+    (void)mvwprintw(datawin, 4,5, "Roll:");
+    (void)mvwprintw(datawin, 5,5, "Dip:");
+    (void)mvwprintw(datawin, 6,5, "GPS Type:");
+    (void)wborder(datawin, 0, 0, 0, 0, 0, 0, 0, 0);
+    (void)mvwprintw(command, 1,1, "Command:  ");
+    (void)wborder(command, 0, 0, 0, 0, 0, 0, 0, 0);
+
+  } else {
+    /* We're a GPS, set up accordingly. */
+
+    /* Grow the windows based on screen size, but not more than the
+       maximum allowed number of sats.  Always leave at least four
+       lines at the bottom of the screen to scroll gpsd raw data. */
+    bigger=ysize-24;
+    
+    if(bigger<0) {
+      bigger=0;
+    }
+    
+    if(bigger > (MAXCHANNELS - 2 - 12)) {
+      bigger = MAXCHANNELS - 2 - 12;
+    }
+    
+    /*@ -onlytrans @*/
+    datawin    = newwin(15+bigger, 45, 1, 0);
+    if(nosats_flag==0) 
+      satellites = newwin(15+bigger, 35, 1, 45);
+    if(nosats_flag==1) {
+      /* Stack them vertically. */
+      command    = newwin(3, 45, 19+bigger, 0);
+      status     = newwin(3, 45, 16+bigger, 0);
+      messages   = newwin(0, 0, 22+bigger, 0);
+    } else {
+      /* Do it normally. */
+      command    = newwin(3, 45, 16+bigger, 0);
+      status     = newwin(3, 35, 16+bigger, 45);
+      messages   = newwin(0, 0, 19+bigger, 0);
+    }
+
+    /*@ +onlytrans @*/
+    (void)scrollok(messages, true);
+    (void)wsetscrreg(messages, 0, LINES-13);
+    (void)nodelay(messages,(bool)TRUE);
+    
+    if(nosats_flag==1) {
+      (void)mvprintw(0, 15, "CGPS Test Client");
+    } else {
+      (void)mvprintw(0, 31, "CGPS Test Client");
+    }
+    /*@ -nullpass @*/
+    (void)refresh();
+    /*@ +nullpass @*/
+    
+    /* Do the initial field label setup. */
+    (void)mvwprintw(datawin, 1,5, "Time:");
+    (void)mvwprintw(datawin, 2,5, "Latitude:");
+    (void)mvwprintw(datawin, 3,5, "Longitude:");
+    (void)mvwprintw(datawin, 4,5, "Altitude:");
+    (void)mvwprintw(datawin, 5,5, "Speed:");
+    (void)mvwprintw(datawin, 6,5, "Heading:");
+    (void)mvwprintw(datawin, 7,5, "Climb:");
+    (void)mvwprintw(datawin, 8,5, "GPS Type:");
+    (void)mvwprintw(datawin, 9,5, "Horizontal Err:");
+    (void)mvwprintw(datawin, 10,5, "Vertical Err:");
+    (void)mvwprintw(datawin, 11,5, "Course Err:");
+    (void)mvwprintw(datawin, 12,5, "Speed Err:");
+    (void)mvwprintw(status, 1,1, "Status:");
+    (void)wborder(datawin, 0, 0, 0, 0, 0, 0, 0, 0);
+    if(nosats_flag==0) {
+      (void)mvwprintw(satellites, 1,1, "PRN:   Elev:  Azim:  SNR:  Used:");
+      (void)wborder(satellites, 0, 0, 0, 0, 0, 0, 0, 0);
+    }
+    (void)mvwprintw(command, 1,1, "Command:  ");
+    (void)wborder(command, 0, 0, 0, 0, 0, 0, 0, 0);
+    (void)wborder(status, 0, 0, 0, 0, 0, 0, 0, 0);
   }
 
-  if(bigger > (MAXCHANNELS - 2 - 12)) {
-    bigger = MAXCHANNELS - 2 - 12;
+  /* Here's where updates go now that things are established. */
+  if(compass_flag==1) {
+    gps_set_raw_hook(gpsdata, update_compass_panel);
+  } else {
+    gps_set_raw_hook(gpsdata, update_gps_panel);
   }
-
-  /*@ -onlytrans @*/
-  datawin    = newwin(15+bigger, 45, 1, 0);
-  satellites = newwin(15+bigger, 35, 1, 45);
-  command    = newwin(3,  45,  16+bigger, 0);
-  status     = newwin(3,  35,  16+bigger, 45);
-  messages   = newwin(0,  0,  19+bigger, 0);
-  /*@ +onlytrans @*/
-  (void)scrollok(messages, true);
-  (void)wsetscrreg(messages, 0, LINES-13);
-  (void)nodelay(messages,(bool)TRUE);
-
-  (void)mvprintw(0, 31, "CGPS Test Client");
-  /*@ -nullpass @*/
-  (void)refresh();
-  /*@ +nullpass @*/
-
-  /* Do the initial field label setup. */
-  (void)mvwprintw(datawin, 1,5, "Time:");
-  (void)mvwprintw(datawin, 2,5, "Latitude:");
-  (void)mvwprintw(datawin, 3,5, "Longitude:");
-  (void)mvwprintw(datawin, 4,5, "Altitude:");
-  (void)mvwprintw(datawin, 5,5, "Speed:");
-  (void)mvwprintw(datawin, 6,5, "Heading:");
-  (void)mvwprintw(datawin, 7,5, "Climb:");
-  (void)mvwprintw(datawin, 9,5, "Horizontal Err:");
-  (void)mvwprintw(datawin, 10,5, "Vertical Err:");
-  (void)mvwprintw(datawin, 11,5, "Course Err:");
-  (void)mvwprintw(datawin, 12,5, "Speed Err:");
-  (void)mvwprintw(status, 1,1, "Status:");
-  (void)wborder(datawin, 0, 0, 0, 0, 0, 0, 0, 0);
-  (void)mvwprintw(satellites, 1,1, "PRN:   Elev:  Azim:  SNR:  Used:");
-  (void)wborder(satellites, 0, 0, 0, 0, 0, 0, 0, 0);
-  (void)mvwprintw(command, 1,1, "Command:  ");
-  (void)wborder(command, 0, 0, 0, 0, 0, 0, 0, 0);
-  (void)wborder(status, 0, 0, 0, 0, 0, 0, 0, 0);
-
-  /* Here's where updates go. */
-  gps_set_raw_hook(gpsdata, update_panel);
-
-  /* If the user requested a specific device, try to change to it. */
-  if (device)
-      (void)gps_query(gpsdata, "F=%s\n", device);
 
   /* Request "w+x" data from gpsd. */
   (void)gps_query(gpsdata, "w+x\n");
@@ -474,7 +673,11 @@ int main(int argc, char *argv[])
         die(1);
     }
 
-    /* Check for user input. */
+    /* Check for user input.
+
+     TODO: Restrict the allowed keys based on device type (ie, 'j'
+     makes no sense for a True North compass).  No doubt there will be
+     other examples as cgps grows more bells and whistles. */
     c=wgetch(messages);
         
     switch ( c ) {
