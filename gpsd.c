972b3bf952b2d1316f8be325927dd9a24db0dad5
Eric S. Raymond <esr@thyrsus.com>
2006-11-17 09:06:31 +0000

    Do a better job of telling gpsd_wrap() that a device has been removed.

--- gpsd.c
+++ gpsd.c
@@ -1164,7 +1164,7 @@ static void handle_control(int sfd, char *buf)
 	    for (cfd = 0; cfd < MAXSUBSCRIBERS; cfd++)
 		if (subscribers[cfd].device == chp)
 		    subscribers[cfd].device = NULL;
-	    chp->gpsdata.gps_fd != -1;	/* device is already disconnected */
+	    chp->gpsdata.gps_fd = -1;	/* device is already disconnected */
 	    gpsd_wrap(chp);
 	    /*@i@*/free_channel(chp);	/* modifying observer storage */
 	    (void)write(sfd, "OK\n", 3);
