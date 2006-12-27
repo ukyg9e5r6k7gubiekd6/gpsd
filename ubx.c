546a4d9fe6a6db19618f81933609d3e32ffeac7a
Chris Kuethe <chris.kuethe@gmail.com>
2006-12-27 06:31:34 +0000

    better tag format

--- ubx.c
+++ ubx.c
@@ -292,7 +292,7 @@ gps_mask_t ubx_parse(struct gps_device_t *session, unsigned char *buf, size_t le
 
     if (mask)
 	(void)snprintf(session->gpsdata.tag, sizeof(session->gpsdata.tag),
-	   "msg%04x",(int)getuw(buf,2));
+	   "0x%04hx", msgid);
 
     return mask | ONLINE_SET;
 }
