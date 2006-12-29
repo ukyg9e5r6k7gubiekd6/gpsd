3e34d23a867e11ca30011b4a02fcd7207384a63a
Chris Kuethe <chris.kuethe@gmail.com>
2006-12-29 05:29:44 +0000

    More iTalk joy.
    Now decodes leap seconds and sv status; navigation solution to follow
    soon.

--- italk.c
+++ italk.c
@@ -18,6 +18,85 @@
 
 #define LITTLE_ENDIAN_PROTOCOL
 #include "bits.h"
+#include "italk.h"
+static gps_mask_t italk_parse(struct gps_device_t *, unsigned char *, size_t);
+static gps_mask_t decode_itk_navfix(struct gps_device_t *, unsigned char *, size_t);
+static gps_mask_t decode_itk_prnstatus(struct gps_device_t *, unsigned char *, size_t);
+static gps_mask_t decode_itk_utcionomodel(struct gps_device_t *, unsigned char *, size_t);
+
+static gps_mask_t decode_itk_navfix(struct gps_device_t *session, unsigned char *buf, size_t len)
+{
+	return 0;
+}
+
+static gps_mask_t decode_itk_prnstatus(struct gps_device_t *session, unsigned char *buf, size_t len)
+{
+    unsigned int tow;
+    unsigned char i, st, nchan, nsv;
+    unsigned short gps_week;
+    double t;
+
+    if (len < 62){
+	gpsd_report(LOG_PROG, "ITALK: runt PRN_STATUS (len=%d)\n", len);
+	return -1;
+    }
+
+    gps_week = getuw(buf, 7 + 4);
+    tow = getul(buf, 7 + 6);
+    t = gpstime_to_unix(gps_week, tow/1000.0) - session->context->leap_seconds;
+    session->gpsdata.sentence_time = session->gpsdata.fix.time = t;
+
+    gpsd_zero_satellites(&session->gpsdata);
+    nchan = (len - 10 - 52) / 20; 
+    st = nsv = 0;
+    for (i = 0; i < nchan; i++) {
+	int off = 7+ 52 + 20 * i;
+	bool good;
+	unsigned short flags;
+	
+	flags = getuw(buf, off);
+	session->gpsdata.used[st] = ((flags & PRN_FLAG_USE_IN_NAV) ? 1 : 0)&0xff;
+	session->gpsdata.ss[st]		= (int)getuw(buf, off+2)&0xff;
+	session->gpsdata.PRN[st]	= (int)getuw(buf, off+4)&0xff;
+	session->gpsdata.elevation[st]	= (int)getsw(buf, off+6)&0xff;
+	session->gpsdata.azimuth[st]	= (int)getsw(buf, off+8)&0xff;
+	good = session->gpsdata.PRN[st]!=0 && 
+	    session->gpsdata.azimuth[st]!=0 && 
+	    session->gpsdata.elevation[st]!=0;
+	if (good!=0)
+	    st++;
+    }
+    session->gpsdata.satellites = st;
+
+    return SATELLITE_SET | TIME_SET;
+}
+
+static gps_mask_t decode_itk_utcionomodel(struct gps_device_t *session, unsigned char *buf, size_t len)
+{
+    unsigned int tow;
+    unsigned short gps_week, flags, leap;
+    double t;
+
+    if (len != 64){
+	gpsd_report(LOG_PROG, "ITALK: bad UTC_IONO_MODEL (len %d, should be 64)\n", len);
+	return -1;
+    }
+
+    flags = getuw(buf, 7);
+    if (0 == (flags & UTC_IONO_MODEL_UTCVALID))
+	return 0;
+
+    leap = getuw(buf, 7 + 24);
+    if (session->context->leap_seconds < leap)
+    	session->context->leap_seconds = leap;
+
+    gps_week = getuw(buf, 7 + 36);
+    tow = getul(buf, 7 + 38);
+    t = gpstime_to_unix(gps_week, tow/1000.0) - session->context->leap_seconds;
+    session->gpsdata.sentence_time = session->gpsdata.fix.time = t;
+
+    return TIME_SET;
+}
 
 /*@ +charint -usedef -compdef @*/
 static bool italk_write(int fd, unsigned char *msg, size_t msglen) {
@@ -41,7 +120,8 @@ static bool italk_write(int fd, unsigned char *msg, size_t msglen) {
 /*@ +charint @*/
 static gps_mask_t italk_parse(struct gps_device_t *session, unsigned char *buf, size_t len)
 {
-    int type;
+    int mask = 0, type;
+
     if (len == 0)
 	return 0;
 
@@ -49,17 +129,86 @@ static gps_mask_t italk_parse(struct gps_device_t *session, unsigned char *buf, 
     /* we may need to dump the raw packet */
     gpsd_report(LOG_RAW, "raw italk packet type 0x%02x length %d: %s\n", type, len, gpsd_hexdump(buf, len));
 
-    (void)snprintf(session->gpsdata.tag, sizeof(session->gpsdata.tag),
-		   "ITK-%02x",type);
-
     switch (type)
     {
-	/* DISPATCH ON FIRST BYTE OF PAYLOAD */
+    case ITALK_NAV_FIX:
+	gpsd_report(LOG_IO, "iTalk NAV_FIX len %d\n", len);
+	mask = decode_itk_navfix(session, buf, len);
+	break;
+    case ITALK_PRN_STATUS:
+	gpsd_report(LOG_IO, "iTalk PRN_STATUS len %d\n", len);
+	mask = decode_itk_prnstatus(session, buf, len);
+	break;
+    case ITALK_UTC_IONO_MODEL:
+	gpsd_report(LOG_IO, "iTalk UTC_IONO_MODEL len %d\n", len);
+	mask = decode_itk_utcionomodel(session, buf, len);
+	break;
 
+    case ITALK_ACQ_DATA:
+	gpsd_report(LOG_IO, "iTalk ACQ_DATA len %d\n", len);
+	break;
+    case ITALK_TRACK:
+	gpsd_report(LOG_IO, "iTalk TRACK len %d\n", len);
+	break;
+    case ITALK_PSEUDO:
+	gpsd_report(LOG_IO, "iTalk PSEUDO len %d\n", len);
+	break;
+    case ITALK_RAW_ALMANAC:
+	gpsd_report(LOG_IO, "iTalk RAW_ALMANAC len %d\n", len);
+	break;
+    case ITALK_RAW_EPHEMERIS:
+	gpsd_report(LOG_IO, "iTalk RAW_EPHEMERIS len %d\n", len);
+	break;
+    case ITALK_SUBFRAME:
+	gpsd_report(LOG_IO, "iTalk SUBFRAME len %d\n", len);
+	break;
+    case ITALK_BIT_STREAM:
+	gpsd_report(LOG_IO, "iTalk BIT_STREAM len %d\n", len);
+	break;
+
+    case ITALK_AGC:
+    case ITALK_SV_HEALTH:
+    case ITALK_PRN_PRED:
+    case ITALK_FREQ_PRED:
+    case ITALK_DBGTRACE:
+    case ITALK_START:
+    case ITALK_STOP:
+    case ITALK_SLEEP:
+    case ITALK_STATUS:
+    case ITALK_ITALK_CONF:
+    case ITALK_SYSINFO:
+    case ITALK_ITALK_TASK_ROUTE:
+    case ITALK_PARAM_CTRL:
+    case ITALK_PARAMS_CHANGED:
+    case ITALK_START_COMPLETED:
+    case ITALK_STOP_COMPLETED:
+    case ITALK_LOG_CMD:
+    case ITALK_SYSTEM_START:
+    case ITALK_STOP_SEARCH:
+    case ITALK_SEARCH:
+    case ITALK_PRED_SEARCH:
+    case ITALK_SEARCH_DONE:
+    case ITALK_TRACK_DROP:
+    case ITALK_TRACK_STATUS:
+    case ITALK_HANDOVER_DATA:
+    case ITALK_CORE_SYNC:
+    case ITALK_WAAS_RAWDATA:
+    case ITALK_ASSISTANCE:
+    case ITALK_PULL_FIX:
+    case ITALK_MEMCTRL:
+    case ITALK_STOP_TASK:
+	gpsd_report(LOG_IO, "iTalk not processing packet: id 0x%02x length %d\n", type, len);
+	break;
     default:
-	gpsd_report(LOG_PROG, "unknown iTalk packet id 0x%02x length %d\n", type, len, gpsd_hexdump(buf, len));
-	return 0;
+	gpsd_report(LOG_IO, "iTalk unknown packet: id 0x%02x length %d\n", type, len);
     }
+    if (mask == -1)
+	mask = 0;
+    else
+	(void)snprintf(session->gpsdata.tag, sizeof(session->gpsdata.tag),
+	   "ITK-%02x",type);
+
+    return mask | ONLINE_SET;
 }
 /*@ -charint @*/
 
