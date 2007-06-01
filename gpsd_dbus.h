7ec8646cc3568e54c7d99d04450d59317db208d4
Eric S. Raymond <esr@thyrsus.com>
2007-06-01 21:17:33 +0000

    More splint cleanups.
    Remove some unneeded file inclusions.

--- gpsd_dbus.h
+++ gpsd_dbus.h
@@ -1,13 +1,10 @@
 /* $Id$ */
-#include "gpsd_config.h"
 #ifdef DBUS_ENABLE
 
 #ifndef _gpsd_dbus_h_
 #define _gpsd_dbus_h_
 
-//#define DBUS_API_SUBJECT_TO_CHANGE
 #include <dbus/dbus.h>
-#include "gpsd.h"
 
 int initialize_dbus_connection (void);
 void send_dbus_fix (struct gps_device_t* channel);
