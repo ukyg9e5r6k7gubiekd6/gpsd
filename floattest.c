aa27aedea610e89ea9946f326cae35b52ca6afc0
Chris Kuethe <chris.kuethe@gmail.com>
2007-12-09 01:25:19 +0000

    run all the tests, even if one fails.
    it might be fun to know just how broken your floating point is
    ... (i'm looking at a certain popular virtualization environment here)

--- floattest.c
+++ floattest.c
@@ -32,55 +32,62 @@ int test_single( void );
 int test_double( void );
 
 int main(){
-	int i;
+	int i, j;
 
-	if ((i = test_single())){
+	if ((i = test_single()))
 		printf("WARNING: Single-precision "
-			"floating point math might be broken (test %d)\n", i);
-		return i;
-	}
+			"floating point math might be broken\n");
 
-	if ((i = test_double())){
+	if ((j = test_double()))
 		printf("WARNING: Double-precision "
-			"floating point math might be broken (test %d)\n", i);
-		return i;
-	}
+			"floating point math might be broken\n");
 
-	printf("floating point math appears to work\n");
-	return 0;
+	i += j;
+	if (i == 0)
+		printf("floating point math appears to work\n");
+	return i;
 }
 
 int test_single(){
 	static float f;
 	static int i;
+	static int e = 0;
 
 	/* addition test */
 	f = 1.0;
 	for(i = 0; i < 10; i++)
 		f += (1<<i);
-	if (f != 1024.0)
-		return 1;
+	if (f != 1024.0){
+		printf("s1 ");
+		e++;
+	}
 
 	/* subtraction test */
 	f = 1024.0;
 	for(i = 0; i < 10; i++)
 		f -= (1<<i);
-	if (f != 1.0)
-		return 2;
+	if (f != 1.0){
+		printf("s2 ");
+		e++;
+	}
 
 	/* multiplication test */
 	f = 1.0;
 	for(i = 1; i < 10; i++)
 		f *= i;
-	if (f != 362880.0)
-		return 3;
+	if (f != 362880.0){
+		printf("s3 ");
+		e++;
+	}
 
 	/* division test */
 	f = 362880.0;
 	for(i = 1; i < 10; i++)
 		f /= i;
-	if (f != 1.0)
-		return 4;
+	if (f != 1.0){
+		printf("s4 ");
+		e++;
+	}
 
 	/* multiply-accumulate test */
 	f = 0.5;
@@ -88,8 +95,10 @@ int test_single(){
 		f += 2.0;
 		f *= 0.5;
 	}
-	if (f != 2.0)
-		return 5;
+	if (f != 2.0){
+		printf("s5 ");
+		e++;
+	}
 
 	/* divide-subtract test */
 	f = 2.0;
@@ -97,72 +106,91 @@ int test_single(){
 		f /= 0.5;
 		f -= 2.0;
 	}
-	if (f != 2.0)
-		return 6;
+	if (f != 2.0){
+		printf("s6 ");
+		e++;
+	}
 
 	/* add-multiply-subtract-divide test */
 	f = 1000000.0;
 	for(i = 1; i < 1000000; i++)
 		f = ((((f + 1.5) * 0.5) - 1.25) / 0.5);
-	if (f != 1.0)
-		return 7;
+	if (f != 1.0){
+		printf("s7 ");
+		e++;
+	}
 
 	/* multiply-add-divide-subtract test */
 	f = 1.0;
 	for(i = 1; i < 1000000; i++)
 		f = ((((f * 5.0) + 3.0) / 2.0) - 3.0);
 	if (f != 1.0)
-		return 8;
+		printf("s8 ");
 
 	/* subtract-divide-add-multiply test */
 	f = 8.0;
 	for(i = 1; i < 1000000; i++)
 		f = ((((f - 5.0) / 2.0) + 2.5) * 2.0);
-	if (f != 8.0)
-		return 9;
+	if (f != 8.0){
+		printf("s9 ");
+		e++;
+	}
 
 	/* divide-subtract-multiply-add test */
 	f = 42.0;
 	for(i = 1; i < 1000000; i++)
 		f = ((((f / 6.0) - 5.0) * 19.75 ) + 2.5);
-	if (f != 42.0)
-		return 10;
+	if (f != 42.0){
+		printf("s10 ");
+		e++;
+	}
+	if (e)
+		printf("\n");
 
-	return 0;
+	return e;
 }
 
 
 int test_double(){
 	static double f;
 	static int i;
+	static int e = 0;
 
 	/* addition test */
 	f = 1.0;
 	for(i = 0; i < 10; i++)
 		f += (1<<i);
-	if (f != 1024.0)
-		return 1;
+	if (f != 1024.0){
+		printf("d1 ");
+		e++;
+	}
 
 	/* subtraction test */
 	f = 1024.0;
 	for(i = 0; i < 10; i++)
 		f -= (1<<i);
-	if (f != 1.0)
-		return 2;
+	if (f != 1.0){
+		printf("d2 ");
+		e++;
+	}
 
 	/* multiplication test */
 	f = 1.0;
 	for(i = 1; i < 10; i++)
 		f *= i;
-	if (f != 362880.0)
-		return 3;
+	if (f != 362880.0){
+		printf("d3 ");
+		e++;
+	}
 
 	/* division test */
 	f = 362880.0;
 	for(i = 1; i < 10; i++)
 		f /= i;
-	if (f != 1.0)
-		return 4;
+	if (f != 1.0){
+		printf("d4 ");
+		e++;
+	}
 
 	/* multiply-accumulate test */
 	f = 0.5;
@@ -170,8 +198,10 @@ int test_double(){
 		f += 2.0;
 		f *= 0.5;
 	}
-	if (f != 2.0)
-		return 5;
+	if (f != 2.0){
+		printf("d5 ");
+		e++;
+	}
 
 	/* divide-subtract test */
 	f = 2.0;
@@ -179,37 +209,46 @@ int test_double(){
 		f /= 0.5;
 		f -= 2.0;
 	}
-	if (f != 2.0)
-		return 6;
+	if (f != 2.0){
+		printf("d6 ");
+		e++;
+	}
 
 	/* add-multiply-subtract-divide test */
 	f = 1000000.0;
 	for(i = 1; i < 1000000; i++)
 		f = ((((f + 1.5) * 0.5) - 1.25) / 0.5);
-	if (f != 1.0)
-		return 7;
+	if (f != 1.0){
+		printf("d7 ");
+		e++;
+	}
 
 	/* multiply-add-divide-subtract test */
 	f = 1.0;
-	for(i = 1; i < 1000000; i++){
+	for(i = 1; i < 1000000; i++)
 		f = ((((f * 5.0) + 3.0) / 2.0) - 3.0);
-	}
 	if (f != 1.0)
-		return 8;
+		printf("d8 ");
 
 	/* subtract-divide-add-multiply test */
 	f = 8.0;
 	for(i = 1; i < 1000000; i++)
 		f = ((((f - 5.0) / 2.0) + 2.5) * 2.0);
-	if (f != 8.0)
-		return 9;
+	if (f != 8.0){
+		printf("d9 ");
+		e++;
+	}
 
 	/* divide-subtract-multiply-add test */
 	f = 42.0;
 	for(i = 1; i < 1000000; i++)
 		f = ((((f / 6.0) - 5.0) * 19.75 ) + 2.5);
-	if (f != 42.0)
-		return 10;
+	if (f != 42.0){
+		printf("d10 ");
+		e++;
+	}
+	if (e)
+		printf("\n");
 
-	return 0;
+	return e;
 }
