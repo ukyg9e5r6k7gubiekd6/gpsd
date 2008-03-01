7c23dbb3aa2391514cc333518dd99075b9a99dc8
Chris Kuethe <chris.kuethe@gmail.com>
2008-03-01 07:30:51 +0000

    allow the SBAS diamond icons to be filled...
    ...if they're used in the navigation solution rather than making them
    be circles.

--- gpsd.php
+++ gpsd.php
@@ -1,5 +1,6 @@
 <?php
 
+# $Id$
 #$CSK: gpsd.php,v 1.39 2006/11/21 22:31:10 ckuethe Exp $
 
 # Copyright (c) 2006 Chris Kuethe <chris.kuethe@gmail.com>
@@ -171,13 +172,15 @@ function splot($im, $sz, $C, $e){
 
 	imageString($im, 3, $x+4, $y+4, $sv, $C['black']);
 	if ($u)
-		imageFilledArc($im, $x, $y, $r, $r, 0, 360, $color, 0);
+		if ($sv > 32)
+			imageFilledDiamond($im, $x, $y, $r, $color);
+		else
+			imageFilledArc($im, $x, $y, $r, $r, 0, 360, $color, 0);
 	else
-		if ($sv > 32) {
+		if ($sv > 32)
 			imageDiamond($im, $x, $y, $r, $color);
-		} else {
+		else
 			imageArc($im, $x, $y, $r, $r, 0, 360, $color);
-		}
 }
 
 function imageDiamond($im, $x, $y, $r, $color){
@@ -192,6 +195,16 @@ function imageDiamond($im, $x, $y, $r, $color){
 	$vx = array ( $x+$t, $y, $x, $y+$t, $x-$t, $y, $x, $y-$t );
 	imagepolygon($im, $vx, 4, $color);
 }
+
+function imageFilledDiamond($im, $x, $y, $r, $color){
+	$t = $r/2;
+	while($t){
+		$vx = array ( $x+$t, $y, $x, $y+$t, $x-$t, $y, $x, $y-$t );
+		imagepolygon($im, $vx, 4, $color);
+		$t -= 0.5;
+	}
+}       
+
 
 function elevation($im, $sz, $C, $a){
 	$b = 90 - $a;
