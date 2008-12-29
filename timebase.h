23a9df1a898fc20f694d28f1f2f5bc3b055a06ce
Eric S. Raymond <esr@thyrsus.com>
2008-12-29 18:07:44 +0000

    Document our exposure if LEAP_SECONDS is wrong.

--- timebase.h
+++ timebase.h
@@ -18,6 +18,21 @@
  *	time.mktime(time.strptime(... , "%d %b %Y %H:%M:%S"))
  * to generate an integer value for START_SUBFRAME. 
  */
+
+/*
+ * This constant is used to get UTC from chipsets that report GPS time only.
+ *
+ * It's not a disaster if it's wrong; most such chips get the offset
+ * from some report abstracted from the subframe data, so their worst
+ * case is their time info will be incorrect for the remainder of an
+ * entire GPS message cycle (about 22 minutes) if you start gpsd up
+ * right after a leap-second.
+ *
+ * This value is only critical if the chipset gets GPS time only and
+ * not the offset; in this case gpsd will always report UTC that is exactly
+ * as incorrect as the constant.  Currently this is true only for the
+ * Evermore chipset.
+ */
 #define LEAP_SECONDS	14
 
 /* IERS says the next leap-second will be inserted at the end of 2008.
