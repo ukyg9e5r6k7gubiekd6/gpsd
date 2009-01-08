10897dfeebd5cb72304f137fb38c29d41b50912a
Chris Kuethe <chris.kuethe@gmail.com>
2009-01-08 17:45:01 +0000

    break apart the geoid test driver from the main library.
    hook it up to the makefile similar to the other _test drivers.
    now builds and packages cleanly

--- geoid.c
+++ geoid.c
@@ -135,38 +135,3 @@ static double fix_minuz(double d)
 {
     return ((d == 0.0) ? 0.0 : d);
 }
-
-#ifdef TESTMAIN
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char **argv)
-{
-    double	lat, lon;
-
-    if (argc != 3) {
-	fprintf(stderr,"Usage: %s lat lon\n",argv[0]);
-	return 1;
-    }	
-
-    lat=atof(argv[1]);
-    lon=atof(argv[2]);
-	
-    if (lat > 90. || lat < -90.)
-    {
-	fprintf(stderr," -90 <= lat=%s(%.f) <= 90 ?\n",argv[1],lat);
-	return 1;
-    }	
-
-    if (lon > 180. || lat < -180.)
-    {
-	fprintf(stderr," -180 <= lon=%s(%.f) <= 180 ?\n",argv[2],lon);
-	return 1;
-    }
-	
-    printf(" lat= %f lon= %f geoid correction= %f\n",
-	   lat, lon, wgs84_separation(lat, lon));
-
-    return 0;
-}
-#endif /* TESTMAIN */
---
+++ geoid_test.c
@@ -0,0 +1,44 @@
+/* $Id: geoid.c 4785 2008-07-20 22:55:32Z ckuethe $ */
+/* 
+ * geoid.c -- ECEF to WGS84 conversions, including ellipsoid-to-MSL height
+ *
+ * Geoid separation code by Oleg Gusev, from data by Peter Dana.
+ * ECEF conversion by Rob Janssen.
+ */
+
+#include <sys/types.h>
+#include <math.h>
+#include "gpsd_config.h"
+#include "gpsd.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    double	lat, lon;
+
+    if (argc != 3) {
+	fprintf(stderr,"Usage: %s lat lon\n",argv[0]);
+	return 1;
+    }
+
+    lat=atof(argv[1]);
+    lon=atof(argv[2]);
+
+    if (lat > 90. || lat < -90.)
+    {
+	fprintf(stderr," -90 <= lat=%s(%.f) <= 90 ?\n",argv[1],lat);
+	return 1;
+    }
+
+    if (lon > 180. || lat < -180.)
+    {
+	fprintf(stderr," -180 <= lon=%s(%.f) <= 180 ?\n",argv[2],lon);
+	return 1;
+    }
+
+    printf(" lat= %f lon= %f geoid correction= %f\n",
+	   lat, lon, wgs84_separation(lat, lon));
+
+    return 0;
+}
