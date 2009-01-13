7d31883b70f908a7e828d9d7b55f3248d024c1df
Eric S. Raymond <esr@thyrsus.com>
2009-01-13 01:42:35 +0000

    Fix buggy termination test,

--- gpspacket.c
+++ gpspacket.c
@@ -71,17 +71,19 @@ Lexer_init(LexerObject *self)
 static PyObject *
 Lexer_get(LexerObject *self, PyObject *args)
 {
-    ssize_t len;
+    ssize_t len, waiting;
     int fd;
 
     if (!PyArg_ParseTuple(args, "i;missing or invalid file descriptor argument to gpspacket.get", &fd))
         return NULL;
 
+    waiting = packet_buffered_input(&self->lexer);
+
     len = packet_get(fd, &self->lexer);
     if (PyErr_Occurred())
 	return NULL;
 
-    if (len == 0) {
+    if (len <= 0 && waiting <= 0) {
 	self->lexer.type = EMPTY_PACKET;
 	self->lexer.outbuffer[0] = '\0';
 	self->lexer.outbuflen = 0;
