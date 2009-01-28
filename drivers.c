1944980847130c620f85f7b7ebe2bdc08f51eda4
Eric S. Raymond <esr@thyrsus.com>
2009-01-28 23:48:49 +0000

    Another correction for the mode-changer code.

--- drivers.c
+++ drivers.c
@@ -269,11 +269,11 @@ static struct gps_type_t nmea = {
 static void garmin_mode_switch(struct gps_device_t *session, int mode)
 /* only does anything iin one direction, going to Garmin binary driver */
 {
-    if (mode == MODE_NMEA) {
+    if (mode == MODE_BINARY) {
 	(void)nmea_send(session, "$PGRMC1,1,2,1,,,,2,W,N");
 	(void)nmea_send(session, "$PGRMI,,,,,,,R");
 	(void)usleep(333);	/* standard Garmin settling time */
-	session->gpsdata.driver_mode = MODE_NMEA;
+	session->gpsdata.driver_mode = MODE_BINARY;
     }
 }
 
