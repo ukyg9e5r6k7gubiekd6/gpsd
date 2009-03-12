80195adfa5a990ac29a72d71583a9e9db49ef09b
Eric S. Raymond <esr@thyrsus.com>
2009-03-12 15:04:37 +0000

    All AIVDM type 1-3 fields except latitude and turn rate now decode correctly.

--- driver_aivdm.c
+++ driver_aivdm.c
@@ -40,77 +40,78 @@
 #pragma pack(1)
 struct aivdm_decode_t
 {
-    unsigned	id:6;		/* message type */
-    unsigned    ri:2;		/* Repeat indicator -- not used */
-    unsigned	mmsi:30;	/* MMSI */		
+    uint	id;		/* message type */
+    uint    	ri;		/* Repeat indicator -- not used */
+    uint	mmsi;	/* MMSI */		
     union {
 	/* Types 1-3 Common navigation info */
 	struct {
-            unsigned status:4;		/* navigation status */
-	    signed rot:8;		/* rate of turn */
+            uint status;		/* navigation status */
+	    signed rot;			/* rate of turn */
 #define AIS_ROT_HARD_LEFT	-127
 #define AIS_ROT_HARD_RIGHT	127
 #define AIS_ROT_NOT_AVAILABLE	128
-	    unsigned sog:10;		/* speed over ground */
+	    double sog;			/* speed over ground */
 #define AIS_SOG_NOT_AVAILABLE	1023
 #define AIS_SOG_FAST_MOVER	1022	/* >= 102.2 knots */
-	    unsigned accuracy:1;	/* position accuracy */
-	    signed longitude:28;	/* longitude */
+	    uint accuracy;		/* position accuracy */
+	    double longitude;		/* longitude */
 #define AIS_LON_NOT_AVAILABLE	181
-	    signed latitude:27;		/* latitude */
+	    double latitude;		/* latitude */
 #define AIS_LAT_NOT_AVAILABLE	91
-	    unsigned cog:12;		/* course over ground */
-	    unsigned heading:9;		/* true heading */
+	    double cog;			/* course over ground */
+#define AIS_COG_NOT_AVAILABLE	3600
+	    uint heading;		/* true heading */
 #define AIS_NO_HEADING	511
-	    unsigned utc_second:6;	/* seconds of UTC timestamp */
+	    uint utc_second;	/* seconds of UTC timestamp */
 #define AIS_SEC_NOT_AVAILABLE	60
 #define AIS_SEC_MANUAL		61
 #define AIS_SEC_ESTIMATED	62
 #define AIS_SEC_INOPERATIVE	63
-	    unsigned regional:3;	/* regional reserved */
+	    uint regional;	/* regional reserved */
 	    /* spares and housekeeping bits follow */ 
 	} type123;
 	/* Type 4 - Base Station Report */
 	struct {
-	    unsigned year:14;		/* UTC year */
+	    uint year;			/* UTC year */
 #define AIS_YEAR_NOT_AVAILABLE	0
-	    unsigned month:4;		/* UTC month */
+	    uint month;			/* UTC month */
 #define AIS_MONTH_NOT_AVAILABLE	0
-	    unsigned day:5;		/* UTC day */
+	    uint day;			/* UTC day */
 #define AIS_DAY_NOT_AVAILABLE	0
-	    unsigned hour:5;		/* UTC hour */
+	    uint hour;			/* UTC hour */
 #define AIS_HOUR_NOT_AVAILABLE	0
-	    unsigned minute:6;		/* UTC minute */
+	    uint minute;		/* UTC minute */
 #define AIS_MINUTE_NOT_AVAILABLE	0
-	    unsigned second:6;		/* UTC second */
+	    uint second;		/* UTC second */
 #define AIS_SECOND_NOT_AVAILABLE	0
-	    unsigned quality:1;		/* fix quality */
-	    signed longitude:28;	/* longitude */
-	    signed latitude:27;		/* latitude */
-	    unsigned epfd_type:4;	/* type of position fix deviuce */
+	    uint quality;		/* fix quality */
+	    signed longitude;		/* longitude */
+	    signed latitude;		/* latitude */
+	    uint epfd_type;		/* type of position fix device */
 	    /* spares and housekeeping bits follow */ 
 	} type4;
 	/* Type 5 - Ship static and voyage related data */
 	struct {
-	    unsigned ais_version:2;	/* AIS version level */
-	    unsigned imo_id:30;		/* IMO identification */
-	    unsigned long long callsign:42;/* callsign as packed sixbit */ 
+	    uint ais_version;			/* AIS version level */
+	    uint imo_id;			/* IMO identification */
+	    unsigned long long callsign:42;	/* callsign as packed sixbit */ 
 	    unsigned long long ship_name1:60;	/* ship name as packed sixbit */
 	    unsigned long long ship_name2:60;	/* more of it... */
-	    unsigned ship_type:8;	/* ship type code */
-	    unsigned to_bow:9;		/* dimension to bow */
-	    unsigned to_stern:9;	/* dimension to stern */
-	    unsigned to_port:6;		/* dimension to port */
-	    unsigned to_starboard:6;	/* dimension to starboard */
-	    unsigned epfd_type:4;	/* type of position fix deviuce */
-	    unsigned month:4;		/* UTC month */
-	    unsigned day:5;		/* UTC day */
-	    unsigned hour:5;		/* UTC hour */
-	    unsigned minute:5;		/* UTC minute */
-	    unsigned draught:9;		/* draft in meters */
+	    uint ship_type;	/* ship type code */
+	    uint to_bow;	/* dimension to bow */
+	    uint to_stern;	/* dimension to stern */
+	    uint to_port;	/* dimension to port */
+	    uint to_starboard;	/* dimension to starboard */
+	    uint epfd_type;	/* type of position fix deviuce */
+	    uint month;		/* UTC month */
+	    uint day;		/* UTC day */
+	    uint hour;		/* UTC hour */
+	    uint minute;	/* UTC minute */
+	    uint draught;	/* draft in meters */
 	    unsigned long long destination1:10;	/* ship destination */
 	    unsigned long long destination2:60;	/* more of it */
-	    unsigned dte:1;
+	    uint dte;
 	    /* spares and housekeeping bits follow */ 
 	} type5;
     };
@@ -143,7 +144,7 @@ gps_mask_t aivdm_parse(struct gps_device_t *session)
     unsigned char *data, *cp = session->driver.aivdm.fieldcopy;    
     unsigned char ch;
     int i;
-    struct aivdm_decode_t *ais; 
+    struct aivdm_decode_t ais; 
 
     if (session->packet.outbuflen == 0)
 	return 0;
@@ -185,7 +186,7 @@ gps_mask_t aivdm_parse(struct gps_device_t *session)
 	    ch = ch - 48;
 	else
 	    ch = ch - 56;
-	gpsd_report(LOG_INF, "%c: %s\n", *cp, sixbits[ch]);
+	gpsd_report(LOG_RAW+1, "%c: %s\n", *cp, sixbits[ch]);
 	for (i = 5; i >= 0; i--) {
 	    if ((ch >> i) & 0x01) {
 		session->driver.aivdm.bits[session->driver.aivdm.bitlen / 8] |= (1 << (7 - session->driver.aivdm.bitlen % 8));
@@ -197,45 +198,82 @@ gps_mask_t aivdm_parse(struct gps_device_t *session)
 
     /* time to pass buffered-up data to where it's actually processed? */
     if (session->driver.aivdm.part == session->driver.aivdm.await) {
+	uint uv;
+	int sv;
+
 	gpsd_report(LOG_IO, "AIVDM payload %ld: %s\n",
 		    session->driver.aivdm.bitlen,
 		    gpsd_hexdump_wrapper(session->driver.aivdm.bits,
 					 (session->driver.aivdm.bitlen + 7)/8, LOG_IO));
 
-	/* the magic moment...*/
-	ais = (struct aivdm_decode_t *)session->driver.aivdm.bits;
 
-	gpsd_report(LOG_INF, "AIVDM message type %d.\n", ais->id);
-	switch (ais->id) {
+#define UBITS(s, l)	ubits((char *)session->driver.aivdm.bits, s, l)
+#define SBITS(s, l)	sbits((char *)session->driver.aivdm.bits, s, l)
+	ais.id = UBITS(0, 6);
+	ais.mmsi = UBITS(8, 30);
+	gpsd_report(LOG_INF, "AIVDM message type %d, MMSI %09d.\n", 
+		    ais.id, ais.mmsi);
+	switch (ais.id) {
 	case 1:	/* Position Report */
 	case 2:
 	case 3:
+	    /* Repeat indicator skipped */
+	    ais.type123.status = UBITS(38, 4);
+	    ais.type123.rot = SBITS(42, 8);
+	    uv = UBITS(50, 10);
+	    if (uv == AIS_SOG_NOT_AVAILABLE)
+		ais.type123.sog = NAN;
+	    else
+		ais.type123.sog = uv / 10.0;
+	    /* position accuracy skipped */
+	    sv = SBITS(61, 28);
+	    if (sv == AIS_LAT_NOT_AVAILABLE)
+		ais.type123.latitude = NAN;
+	    else
+		ais.type123.latitude = sv / 600000.0;
+	    sv = SBITS(89, 27);
+	    if (sv == AIS_LON_NOT_AVAILABLE)
+		ais.type123.longitude = NAN;
+	    else
+		ais.type123.longitude = sv / 600000.0;
+	    uv = UBITS(116, 12);
+	    if (uv == AIS_COG_NOT_AVAILABLE)
+		ais.type123.cog = NAN;
+	    else
+		ais.type123.cog = uv / 10.0;
+	    ais.type123.heading = UBITS(128, 9);
+	    ais.type123.utc_second = UBITS(137, 6);
+	    /* regional and spare */
 	    gpsd_report(LOG_INF,
-			"MMSI: %09d  Latitude: %.7f  Longitude: %.7f  Speed: %f  Course:%d  Heading: %d  Turn: %d\n",
-			ais->mmsi, 
-			ais->type123.latitude / 600000.0, 
-			ais->type123.longitude / 600000.0, 
-			ais->type123.sog / 10.0, 
-			ais->type123.cog, 
-			ais->type123.heading, 
-			ais->type123.rot);
-	    break;  
+			"Latitude: %.7f  Longitude: %.7f  Status=%d  Speed: %.1f  Course: %.1f  Heading: %d  Turn: %d  Second: %d\n",
+			ais.type123.latitude, 
+			ais.type123.longitude, 
+			ais.type123.status,
+			ais.type123.sog, 
+			ais.type123.cog, 
+			ais.type123.heading, 
+			ais.type123.rot,
+			ais.type123.utc_second);
+	    break;
+#if 0
 	case 4:	/* Base Station Report */
 	    gpsd_report(LOG_INF,
-			"MMSI: %09d  Latitude: %.7f  Longitude: %.7f\n",
-			ais->mmsi, 
-			ais->type4.latitude / 600000.0, 
-			ais->type4.longitude / 600000.0);
+			"Latitude: %.7f  Longitude: %.7f\n",
+			ais.type4.latitude / 600000.0, 
+			ais.type4.longitude / 600000.0);
 	    break;
 	case 5: /* Ship static and voyage related data */
 	    gpsd_report(LOG_INF,
 			"IMO: %d\n",
-			ais->type5.imo_id);
+			ais.type5.imo_id);
 	    break;
+#endif
 	default:
-	    gpsd_report(LOG_ERROR, "Unparsed AIVDM message type %d.\n",ais->id);
+	    gpsd_report(LOG_ERROR, "Unparsed AIVDM message type %d.\n",ais.id);
 	    break;
 	} 
+#undef SBITS
+#undef UBITS
 
        /*
 	* XXX The tag field is only 8 bytes; be careful you do not overflow.
@@ -296,4 +334,3 @@ const struct gps_type_t aivdm = {
     .wrapup           = NULL,
 };
 #endif /* defined(AIVDM_ENABLE) */
-
