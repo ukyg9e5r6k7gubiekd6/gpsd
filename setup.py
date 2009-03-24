0c0b0505a797a0af489eb192fb8c8f3036a54c6b
Eric S. Raymond <esr@thyrsus.com>
2009-03-24 15:46:39 +0000

    Python build patches by Richard Hansen ("I" in this comment):
    This patch modifies Makefile.am and setup.py so that 'make distcheck'
    passes.  This patch makes it possible to do VPATH builds (putting build
    products in a separate directory from the unpacked tarball).
    
    Note that I have NOT tested dist-rpm.
    
    The most awkward part of this patch is the changes to setup.py. Python's
    distutils package does not have built-in support for VPATH builds, which
    significantly complicates things.  Interestingly, Python's own build system
    supposedly supports VPATH builds even though it uses distutils.  Python's
    setup.py script subclasses several of the distutils packages, so the VPATH
    magic must happen somewhere in those hundreds of lines of code.  I'll
    investigate further when I have the time.  For now, the changes to setup.py
    are fairly minimal and should work.
    
    This patch also causes setup.py to exit with a non-zero return code if the call
    to make fails, and adds missing dependencies related to the Python modules.

--- setup.py
+++ setup.py
@@ -7,20 +7,34 @@ from distutils.core import setup, Extension
 import os
 import sys
 
-needed_files = ['packet_names.h', 'gpsfake.1', 'gpsprof.1']
+# For VPATH builds, this script must be run from $(srcdir) with the
+# abs_builddir environment variable set to the location of the build
+# directory.  This is necessary because Python's distutils package
+# does not have built-in support for VPATH builds.
+
+# These dependencies are enforced here and not in the Makefile to make
+# it easier to build the Python parts without building everything else
+# (the user can run 'python setup.py' without having to run 'make').
+needed_files = ['gpsd.h', 'packet_names.h', 'gpsfake.1', 'gpsprof.1']
 created_files = []
 
 if not 'clean' in sys.argv:
-    if not os.path.exists('gpsd_config.h'):
+    abs_builddir = os.environ["abs_builddir"] if "abs_builddir" in os.environ else ""
+    if not os.path.exists(os.path.join(abs_builddir, 'gpsd_config.h')):
         sys.stderr.write('\nPlease run configure first!\n')
         sys.exit(1)
 
+    cdcmd = ("cd '" + abs_builddir + "' && ") if abs_builddir else ""
     for f_name in needed_files:
-        if not os.path.exists(f_name):
-            make_in, make_out = os.popen2('make %s' % f_name)
+        # TODO:  Shouldn't make be run unconditionally in case a
+        # dependency of f_name has been updated?
+        if not os.path.exists(os.path.join(abs_builddir, f_name)):
+            cmd = cdcmd + "make '" + f_name + "'"
+            print cmd
+            make_out = os.popen(cmd)
             print make_out.read()
-            make_out.close()
-            make_in.close()
+            if make_out.close():
+                sys.exit(1)
             created_files.append(f_name)
 
 gpspacket_sources = ["gpspacket.c", "packet.c", "isgps.c",
