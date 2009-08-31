e88e2494f62e96bd553bbe5bea8e6a5b03eac5db
Eric S. Raymond <esr@thyrsus.com>
2009-08-31 16:57:46 +0000

    Fix bug in RTCM2 dumping.  All regression tests pass.

--- gpsd_json.c
+++ gpsd_json.c
@@ -414,7 +414,7 @@ void rtcm2_json_dump(struct rtcm2_t *rtcm, /*@out@*/char buf[], size_t buflen)
 	break;
     case 16:
 	(void)snprintf(buf + strlen(buf), buflen - strlen(buf),
-		       "\"message\":\"%s\"", json_stringify(buf, sizeof(buf1), rtcm->message));
+		       "\"message\":\"%s\"", json_stringify(buf1, sizeof(buf1), rtcm->message));
 	break;
 
     default:
