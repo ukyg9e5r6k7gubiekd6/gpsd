922975d6696c766a5c96dd3017d026ef9295178e
Eric S. Raymond <esr@thyrsus.com>
2009-09-17 22:13:02 +0000

    Splint cleanup.  All regression tests pass.

--- pseudonmea.c
+++ pseudonmea.c
@@ -226,10 +226,12 @@ static void gpsd_binary_quality_dump(struct gps_device_t *session,
 #undef ZEROIZE
 }
 
+/*@-compdef -mustdefine@*/
 void gpsd_pseudonmea_dump(struct gps_device_t *session,
-			      char bufp[], size_t len)
+			      /*@out@*/char bufp[], size_t len)
 /* the only entry point - dump a device state in pseudo-NMEA */
 {
+    bufp[0] = '\0';
     if ((session->gpsdata.set & LATLON_SET) != 0) {
 	gpsd_position_fix_dump(session, bufp, len);
 	gpsd_transit_fix_dump(session, bufp + strlen(bufp), len - strlen(bufp));
@@ -239,6 +241,7 @@ void gpsd_pseudonmea_dump(struct gps_device_t *session,
     if ((session->gpsdata.set & SATELLITE_SET) != 0)
 	gpsd_binary_satellite_dump(session,bufp+strlen(bufp),len-strlen(bufp));
 }
+/*@+compdef +mustdefine@*/
 
 /* pseudonmea.c ends here */
 
