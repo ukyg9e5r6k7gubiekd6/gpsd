85d8c8f76711d90a4a0a491d35e5d4632ee44da9
Eric S. Raymond <esr@thyrsus.com>
2009-09-28 19:22:01 +0000

    Un-invert a test.

--- driver_sirf.c
+++ driver_sirf.c
@@ -762,7 +762,7 @@ gps_mask_t sirf_parse(struct gps_device_t *session, unsigned char *buf, size_t l
     switch (buf[0])
     {
     case 0x02:		/* Measure Navigation Data Out */
-	if (session->driver.sirf.driverstate & UBLOX)
+	if ((session->driver.sirf.driverstate & UBLOX)==0)
 	    return sirf_msg_navsol(session, buf, len);
 	else {
 	    gpsd_report(LOG_PROG, "MND 0x02 skipped, uBlox flag is on. %0x\n", session->driver.sirf.driverstate);
