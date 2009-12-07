f56b87a27723861744a6c6e1bbc2dcc9a59b1364
Michael Ledford <michael@ledford.cc>
2009-12-07 03:38:45 +0000

    Fixes a problem on Mac OS X 10.6 with the shared library linking.
    It chose the default gpsd_report() in gpsd_report.c function instead of
    the one provided by library clients. OS X 10.5 didn't have this problem.

--- gpsd_report.c
+++ gpsd_report.c
@@ -4,7 +4,13 @@
 #include <stdarg.h>
 #include "gpsd.h"
 
+
+# if __GNUC__ >= 3 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 7)
+void gpsd_report(int errlevel UNUSED, const char *fmt, ... ) __attribute__ ((weak, alias ("__gpsd_report")));
+void __gpsd_report(int errlevel UNUSED, const char *fmt, ... )
+# else
 void gpsd_report(int errlevel UNUSED, const char *fmt, ... )
+#endif
 /* stub logger for clients that don't supply one */
 {
     va_list ap;
@@ -13,4 +19,3 @@ void gpsd_report(int errlevel UNUSED, const char *fmt, ... )
     (void)vfprintf(stderr, fmt, ap);
     va_end(ap);
 }
-
