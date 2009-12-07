276b1837a91b3f2faeb04980880291976c7a6b70
Michael Ledford <michael@ledford.cc>
2009-12-07 20:29:57 +0000

    Fixes up previous commit that fixed logging function linkage
    ...using the shared library on OS X 10.6. Previous commit caused error
    warnings to be emitted. Plus Mach-O doesn't support aliasing so this
    also removes the alias attribute.

--- gpsd_report.c
+++ gpsd_report.c
@@ -6,11 +6,10 @@
 
 
 # if __GNUC__ >= 3 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 7)
-void gpsd_report(int errlevel UNUSED, const char *fmt, ... ) __attribute__ ((weak, alias ("__gpsd_report")));
-void __gpsd_report(int errlevel UNUSED, const char *fmt, ... )
-# else
-void gpsd_report(int errlevel UNUSED, const char *fmt, ... )
+void gpsd_report(int errlevel UNUSED, const char *fmt, ... ) __attribute__ ((weak));
 #endif
+
+void gpsd_report(int errlevel UNUSED, const char *fmt, ... )
 /* stub logger for clients that don't supply one */
 {
     va_list ap;
