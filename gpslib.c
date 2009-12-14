aa2556909de863fe16a1e7364148aa2349e5ed5a
Eric S. Raymond <esr@thyrsus.com>
2009-12-14 10:01:14 +0000

    Begin upgrading the gpslib stub to export C finctions needed by the Python test client.

--- gpslib.c
+++ gpslib.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include "gps.h"
+#include "gpsdclient.h"
 
 static PyObject *
 gpslib_wgs84_separation(PyObject *self, PyObject *args)
@@ -19,11 +20,24 @@ gpslib_wgs84_separation(PyObject *self, PyObject *args)
     return Py_BuildValue("d", sep);
 }
 
+static PyObject *
+gpslib_deg_to_str(PyObject *self, PyObject *args)
+{
+    int fmt;
+    double degrees;
+
+    if (!PyArg_ParseTuple(args, "id", &fmt, &degrees))
+	return NULL;
+    return Py_BuildValue("s", deg_to_str((enum deg_str_type)fmt, degrees));
+}
+
 /* List of functions defined in the module */
 
 static PyMethodDef gpslib_methods[] = {
     {"wgs84_separation",	gpslib_wgs84_separation,	METH_VARARGS,
-     PyDoc_STR("Return WGS84 geidetic separation in meters.")},
+     PyDoc_STR("Return WGS84 geodetic separation in meters.")},
+    {"deg_to_str",      	gpslib_deg_to_str,      	METH_VARARGS,
+     PyDoc_STR("String-format a latitude/longitude.")},
     {NULL,		NULL}		/* sentinel */
 };
 
@@ -34,5 +48,12 @@ PyDoc_STRVAR(module_doc,
 PyMODINIT_FUNC
 initgpslib(void)
 {
-    Py_InitModule3("gpslib", gpslib_methods, module_doc);
+    PyObject *m;
+
+    m = Py_InitModule3("gpslib", gpslib_methods, module_doc);
+
+    PyModule_AddIntConstant(m, "deg_dd", deg_dd);
+    PyModule_AddIntConstant(m, "deg_ddmm", deg_ddmm);
+    PyModule_AddIntConstant(m, "deg_ddmmss", deg_ddmmss);
 }
+
