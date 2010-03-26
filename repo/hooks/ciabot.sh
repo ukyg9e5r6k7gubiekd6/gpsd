dbc8645ca5501ff433988c5e18b6771215eb2861
Eric S. Raymond <esr@thyrsus.com>
2010-03-26 08:43:29 -0400

    Document the ciabot.sh variables a bit better.

--- repo/hooks/ciabot.sh
+++ repo/hooks/ciabot.sh
@@ -2,9 +2,11 @@
 # Distributed under the terms of the GNU General Public License v2
 # Copyright (c) 2006 Fernando J. Pereda <ferdy@gentoo.org>
 # Copyright (c) 2008 Natanael Copa <natanael.copa@gmail.com>
+# Copyright (c) 2010 Eric S. Raymond <esr@thyrsus.com>
 #
 # Git CIA bot in bash. (no, not the POSIX shell, bash).
 # It is *heavily* based on Git ciabot.pl by Petr Baudis.
+# This script contains porcelain and porcelain byproducts.
 #
 # It is meant to be run either on a post-commit hook or in an update
 # hook:
@@ -25,18 +27,26 @@
 # The project as known to CIA
 project="GPSD"
 repo="${REPO:-gpsd}"
+echo "REPO=$REPO"
 
 # Addresses for the e-mail
 from="esr@thyrsus.com"
 to="cia@cia.vc"
 
-# SMTP client to use
+# Changeset URL prefix for your repo: when the commit ID is appended
+# to this, it should point at a CGI that will display the commit
+# through gitweb or something similar.
+urlprefix="http://git.berlios.de/cgi-bin/gitweb.cgi?p=$repo;a=commit;h="
+
+# SMTP client to use - may need to edit the absolute pathname for your system
 sendmail="/usr/sbin/sendmail -t -f ${from}"
 
-# Changeset URL
-urlprefix="http://git.alpinelinux.org/cgit/$repo/commit/?id="
+#
+# No user-serviceable parts below this line
+#
 
-# You shouldn't be touching anything else.
+PATH="$PATH:`git --exec-path`
+
 if [ $# -eq 0 ] ; then
 	refname=$(git symbolic-ref HEAD 2>/dev/null)
 	merged=$(git rev-parse HEAD)
