e3a306ed0168380ff0107577d6199dd874144b92
Bernd Zeimetz <bernd@bzed.de>
2010-04-22 01:51:45 +0200

    There are utf8 chars in the file, so let Python handle them.

--- gps/gps.py
+++ gps/gps.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
 # This file is Copyright (c) 2010 by the GPSD project
 # BSD terms apply: see the file COPYING in the distribution root for details.
