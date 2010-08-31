60938a487bfc0a9361996f8660dc93bf2e10cec8
Bernd Zeimetz <bernd@bzed.de>
2010-08-31 22:08:42 +0200

    Add script to print Debian buildd regression test failures.

---
+++ contrib/gpsd-debian-regressions.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This file is Copyright (c)2010 by the GPSD project
+# BSD terms apply: see the file COPYING in the distribution root for details.
+
+set -e
+
+if [ ! -x /usr/bin/getbuildlog ]; then
+	echo 'Please install the devscripts package!'
+	exit 1
+fi
+
+TMPDIR=`mktemp -d`
+OLDPWD=`pwd`
+
+cd ${TMPDIR}
+getbuildlog gpsd last 1>/dev/null 2>&1
+grep -- '--- ./test' * | sed 's,^gpsd_[^_]*_\([^.]*\).*\./test/\([^.]*\).*,\1 \2,' | sort -u
+cd ${OLDPWD}
+rm -rf ${TMPDIR}
+
