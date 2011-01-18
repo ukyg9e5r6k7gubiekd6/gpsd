bea3751b6f09ba15864cb44a3584cc0af5e0dfe3
Eric S. Raymond <esr@thyrsus.com>
2011-01-18 17:02:54 -0500

    Stop making an assumption about the leap-second offset at startup.
    Gains: the stored leap-second offset we used for this could go stale,
    breaking our regression tests in the process - itr's just dumb luck
    that it hasn't since done so since 2008.
    
    Losses: If the receiver doesn't have leap-second cached in NVRAM (e.g,
    between cold boot and the next subframe message) time will be inaccurate
    by a few more seconds.
    
    10 regression test outputs of 66 had to be rebuilt.

--- timebase.h
+++ timebase.h
@@ -4,22 +4,6 @@
 /* timebase.h -- constants that will require patching over time */
 
 /*
- * This constant is used to get UTC from chipsets that report GPS time only.
- *
- * It's not a disaster if it's wrong; most such chips get the offset
- * from some report abstracted from the subframe data, so their worst
- * case is their time info will be incorrect for the remainder of an
- * entire GPS message cycle (about 22 minutes) if you start gpsd up
- * right after a leap-second.
- *
- * This value is only critical if the chipset gets GPS time only and
- * not the offset; in this case gpsd will always report UTC that is exactly
- * as incorrect as the constant.  Currently this is true only for the
- * Evermore chipset.
- */
-#define LEAP_SECONDS	15
-
-/*
  * Default century.  Gets used if the system clock value at startup
  * time looks invalid.
  */
