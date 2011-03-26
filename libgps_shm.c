749f86a7b107c7ec08f1e9065ffb6bd83abc7db3
Eric S. Raymond <esr@thyrsus.com>
2011-03-26 10:42:03 -0400

    Plant warnings about odd memcpy() implementations.

--- libgps_shm.c
+++ libgps_shm.c
@@ -55,6 +55,16 @@ int gps_shm_read(struct gps_data_t *gpsdata)
 	struct shmexport_t *shared = (struct shmexport_t *)gpsdata->privdata;
 
 	before = shared->bookend1;
+	/*
+	 * The bookend-consistency technique wants this to be a forward copy.
+	 * But it is not guaranteed, that memcpy() works this way, and some compilers on some platforms
+	 * are known to implement it as a reverse copy.  Notably GCC does
+	 * this on x64 Atom. 
+	 *
+	 * The safest thing to do here would be to use a naive C implementation
+	 * of forward byte copy, but if we did that we'd be sacrificing the 
+	 * superior performance of the asm optimization GCC does for 
+	 */
 	(void)memcpy((void *)gpsdata, 
 		     (void *)&shared->gpsdata, 
 		     sizeof(struct gps_data_t));
