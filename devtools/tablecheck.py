f24d3b8eab794e3afac5c8af0bfec87a4560a674
Eric S. Raymond <esr@thyrsus.com>
2011-04-19 16:14:57 -0400

    Steps towards turning tablecheck.py into an actual code generator.

--- devtools/tablecheck.py
+++ devtools/tablecheck.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
 #
-# Compute offsets for an AIVDM message breakdown, given the bit widths.
 # Takes a single argument, the line number of a table start.
-# Writes the corrected table to standard output.
 
-import sys
+import sys, getopt
 
 if __name__ == '__main__':
-    startline = int(sys.argv[1])
+    try:
+        (options, arguments) = getopt.getopt(sys.argv[1:], "tc")
+    except getopt.GetoptError, msg:
+        print "tablecheck.py: " + str(msg)
+        raise SystemExit, 1
+    generate = maketable = False
+    for (switch, val) in options:
+        if switch == '-c':
+            generate = True     # Logic for this is not yet implemented.
+        elif switch == '-t':
+            maketable = True
 
+    if not generate and not maketable:
+        print >>sys.stderr, "tablecheck.py: no mode selected"
+        sys.exit(1)
+
+    # First, read in the table
+    startline = int(arguments[0])
     table = []
     keep = False
     i = 0
@@ -25,9 +39,9 @@ if __name__ == '__main__':
         if keep:
             table.append(line)
     table = table[2:]
-
     widths = map(lambda s: s.split('|')[2].strip(), table)
 
+    # Compute offsets for an AIVDM message breakdown, given the bit widths.
     offsets = []
     base = 0
     for w in widths:
@@ -41,9 +55,11 @@ if __name__ == '__main__':
     owidth = max(*map(len, offsets)) 
     for (i, off) in enumerate(offsets):
         offsets[i] += " " * (owidth - len(offsets[i]))
-    
-    for (i, t) in enumerate(table):
-        print "|" + offsets[i] + t[owidth+1:].rstrip()
+
+    if maketable:
+        # Writes the corrected table to standard output.
+        for (i, t) in enumerate(table):
+            print "|" + offsets[i] + t[owidth+1:].rstrip()
 
 
     
