503a8db7c89882cece03364bde042c76b018ae81
Eric S. Raymond <esr@thyrsus.com>
2011-04-19 23:30:49 -0400

    Teach the code generator to make structures.

--- devtools/tablecheck.py
+++ devtools/tablecheck.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python
 #
 # Takes a single argument, the line number of a table start.
+# Generates various useful code snippets from tables in the
+# AIVDM descriptions, or corrects offsets in the tables themselves. 
 
 import sys, getopt
 
 if __name__ == '__main__':
     try:
-        (options, arguments) = getopt.getopt(sys.argv[1:], "tc:")
+        (options, arguments) = getopt.getopt(sys.argv[1:], "tc:s:")
     except getopt.GetoptError, msg:
         print "tablecheck.py: " + str(msg)
         raise SystemExit, 1
-    generate = maketable = False
+    generate = maketable = makestruct = False
     for (switch, val) in options:
         if switch == '-c':
             generate = True     # Logic for this is not yet implemented.
-            prefix = val
+            structname = val
+        elif switch == '-s':
+            makestruct = True
+            structname = val
         elif switch == '-t':
             maketable = True
 
-    if not generate and not maketable:
+    if not generate and not maketable and not makestruct:
         print >>sys.stderr, "tablecheck.py: no mode selected"
         sys.exit(1)
 
@@ -61,6 +66,7 @@ if __name__ == '__main__':
     for (i, off) in enumerate(offsets):
         offsets[i] += " " * (owidth - len(offsets[i]))
 
+    # Here's where we generate useful output.
     if maketable:
         # Writes the corrected table to standard output.
         print >>sys.stderr, "Total bits:", base 
@@ -70,7 +76,8 @@ if __name__ == '__main__':
             else:
                 print t.rstrip()
     elif generate:
-        # Writes calls to bit-extraction macros to standard output
+        # Writes calls to bit-extraction macros to standard output.
+        # Requites UBITS and SBITS to act as they do in the AIVDM driver.
         for (i, t) in enumerate(table):
             if '|' in t:
                 fields = map(lambda s: s.strip(), t.split('|'))
@@ -82,8 +89,31 @@ if __name__ == '__main__':
                 elif ftype in ('u', 'i'):
                     offset = offsets[i].split('-')[0]
                     print "\t%s.%s\t= %sBITS(%s, %s);" % \
-                          (prefix, name, {'u':'U', 'i':'S'}[ftype], offset, width)
+                          (structname, name, {'u':'U', 'i':'S'}[ftype], offset, width)
                 else:
                     print "\t/* %s bits of type %s */" % (width, ftype)
+    elif makestruct:
+        # Write a structure definition correponding to the table.
+        print "\tstruct {"
+        for (i, t) in enumerate(table):
+            if '|' in t:
+                fields = map(lambda s: s.strip(), t.split('|'))
+                description = fields[3].strip()
+                name = fields[4]
+                ftype = fields[5]
+                if ftype == 'x':
+                    continue
+                if ftype == 'u':
+                    print "\t\tunsigned int %s;\t/* %s*/" % (name, description)
+                elif ftype == 'i':
+                    print "\t\tint %s;\t/* %s*/" % (name, description)
+                elif ftype == 'b':
+                    print "\t\tint %s;\t/* %s*/" % (name, description)
+                elif ftype == 't':
+                    print "\t\tchar %s[];\t/* %s*/" % (name, description)
+                else:
+                    print "\t/* %s bits of type %s */" % (width, ftype)
+        print "\t} %s;" % structname
 
-    
+# end
+  
