6c19639b6f8c0e465ae1df5138f0948f1fe8d839
Eric S. Raymond <esr@thyrsus.com>
2011-08-18 23:10:06 -0400

    Explanatory comment and upstream bug explanation.

--- test_gpsmm.cpp
+++ test_gpsmm.cpp
@@ -13,6 +13,12 @@
 
 using namespace std;
 
+/* 
+ * We should get libgps_dump_state() from the client library, but
+ * scons has a bug; we can't get it to add -lgps to the link line,
+ * apparently because it doesn't honor parse_flags on a Program()
+ * build of a C++ file.
+ */
 static void libgps_dump_state(struct gps_data_t *collect)
 {
     /* no need to dump the entire state, this is a sanity check */
