5b1dbd86a50a6f25f2a4a8726547a88f3e6e4fa1
Eric S. Raymond <esr@thyrsus.com>
2011-08-23 17:05:49 -0400

    Now that fusecount() is working, it turns out to be dangerous. Disable it...
    ...until we figure out what's going on.  All regression tests pass.

--- serial.c
+++ serial.c
@@ -68,7 +68,7 @@ static sourcetype_t gpsd_classify(const char *path)
 	return source_unknown;
 }
 
-#ifdef __linux__
+#ifdef __UNUSED__
 #include <dirent.h>
 #include <ctype.h>
 
@@ -97,7 +97,7 @@ static int fusercount(const char *path)
 		continue;
 	    if (strcmp(linkpath, path) == 0) {
 		(void)closedir(procd);
-		(void)closedir(fdd);
+		//(void)closedir(fdd);
 		++cnt;
 	    }
 	}
@@ -107,7 +107,7 @@ static int fusercount(const char *path)
 
     return cnt;
 }
-#endif /* __linux__ */
+#endif /* __UNUSED__ */
 
 void gpsd_tty_init(struct gps_device_t *session)
 /* to be called on allocating a device */
@@ -413,7 +413,7 @@ int gpsd_serial_open(struct gps_device_t *session)
 	 */
 	(void)ioctl(session->gpsdata.gps_fd, (unsigned long)TIOCEXCL);
 
-#ifdef __linux__
+#ifdef __UNSAFE__
 	/*
 	 * Don't touch devices already opened by another process.
 	 */
@@ -425,7 +425,7 @@ int gpsd_serial_open(struct gps_device_t *session)
 	    session->gpsdata.gps_fd = -1;
 	    return -1;
 	}
-#endif /* __linux__ */
+#endif /* __UNSAFE__ */
     }
 
 #ifdef FIXED_PORT_SPEED
