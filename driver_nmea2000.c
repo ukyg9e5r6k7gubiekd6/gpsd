50bf6d12cba03715b085ccc2f9df90e71e97ac4a
Eric S. Raymond <esr@thyrsus.com>
2012-05-02 15:36:33 -0400

    Have a single point of truth about NMEA2000 handler declaration.

--- driver_nmea2000.c
+++ driver_nmea2000.c
@@ -43,79 +43,6 @@ typedef struct PGN
 FILE *logFile = NULL;
 #endif /* of if LOG_FILE */
 
-gps_mask_t hnd_059392(unsigned char*, int len, PGN *pgn, struct gps_device_t *session);
-gps_mask_t hnd_060928(unsigned char*, int len, PGN *pgn, struct gps_device_t *session);
-gps_mask_t hnd_126208(unsigned char*, int len, PGN *pgn, struct gps_device_t *session);
-gps_mask_t hnd_126464(unsigned char*, int len, PGN *pgn, struct gps_device_t *session);
-gps_mask_t hnd_126996(unsigned char*, int len, PGN *pgn, struct gps_device_t *session);
-gps_mask_t hnd_126992(unsigned char*, int len, PGN *pgn, struct gps_device_t *session);
-gps_mask_t hnd_129025(unsigned char*, int len, PGN *pgn, struct gps_device_t *session);
-gps_mask_t hnd_129026(unsigned char*, int len, PGN *pgn, struct gps_device_t *session);
-gps_mask_t hnd_129029(unsigned char*, int len, PGN *pgn, struct gps_device_t *session);
-gps_mask_t hnd_129038(unsigned char*, int len, PGN *pgn, struct gps_device_t *session);
-gps_mask_t hnd_129039(unsigned char*, int len, PGN *pgn, struct gps_device_t *session);
-gps_mask_t hnd_129040(unsigned char*, int len, PGN *pgn, struct gps_device_t *session);
-gps_mask_t hnd_129539(unsigned char*, int len, PGN *pgn, struct gps_device_t *session);
-gps_mask_t hnd_129540(unsigned char*, int len, PGN *pgn, struct gps_device_t *session);
-gps_mask_t hnd_129794(unsigned char*, int len, PGN *pgn, struct gps_device_t *session);
-gps_mask_t hnd_129798(unsigned char*, int len, PGN *pgn, struct gps_device_t *session);
-gps_mask_t hnd_129802(unsigned char*, int len, PGN *pgn, struct gps_device_t *session);
-gps_mask_t hnd_129809(unsigned char*, int len, PGN *pgn, struct gps_device_t *session);
-gps_mask_t hnd_129810(unsigned char*, int len, PGN *pgn, struct gps_device_t *session);
-
-/*@-usereleased@*/
-const char msg_059392[] = {"ISO  Acknowledgment"};
-const char msg_060928[] = {"ISO  Address Claim"};
-const char msg_126208[] = {"NMEA Command/Request/Acknowledge"};
-const char msg_126464[] = {"ISO  Transmit/Receive PGN List"};
-const char msg_126992[] = {"GNSS System Time"};
-const char msg_126996[] = {"ISO  Product Information"};
-const char msg_129025[] = {"GNSS Position Rapid Update"};
-const char msg_129026[] = {"GNSS COG and SOG Rapid Update"};
-const char msg_129029[] = {"GNSS Positition Data"};
-const char msg_129539[] = {"GNSS DOPs"};
-const char msg_129540[] = {"GNSS Satellites in View"};
-const char msg_129038[] = {"AIS  Class A Position Report"};
-const char msg_129039[] = {"AIS  Class B Position Report"};
-const char msg_129040[] = {"AIS  Class B Extended Position Report"};
-const char msg_129794[] = {"AIS  Class A Static and Voyage Related Data"};
-const char msg_129798[] = {"AIS  SAR Aircraft Position Report"};
-const char msg_129802[] = {"AIS  Safty Related Broadcast Message"};
-const char msg_129809[] = {"AIS  Class B CS Static Data Report, Part A"};
-const char msg_129810[] = {"AIS  Class B CS Static Data Report, Part B"};
-const char msg_error [] = {"**error**"};
-
-
-PGN gpspgn[] = {{ 59392, 0, 0, hnd_059392, &msg_059392[0]},
-		{ 60928, 0, 0, hnd_060928, &msg_060928[0]},
-		{126208, 0, 0, hnd_126208, &msg_126208[0]},
-		{126464, 1, 0, hnd_126464, &msg_126464[0]},
-		{126992, 0, 0, hnd_126992, &msg_126992[0]},
-		{126996, 1, 0, hnd_126996, &msg_126996[0]},
-		{129025, 0, 1, hnd_129025, &msg_129025[0]},
-		{129026, 0, 1, hnd_129026, &msg_129026[0]},
-		{129029, 1, 1, hnd_129029, &msg_129029[0]},
-		{129539, 0, 1, hnd_129539, &msg_129539[0]},
-		{129540, 1, 1, hnd_129540, &msg_129540[0]},
-		{0     , 0, 0, NULL,       &msg_error [0]}};
-
-PGN aispgn[] = {{ 59392, 0, 0, hnd_059392, &msg_059392[0]},
-		{ 60928, 0, 0, hnd_060928, &msg_060928[0]},
-		{126208, 0, 0, hnd_126208, &msg_126208[0]},
-		{126464, 1, 0, hnd_126464, &msg_126464[0]},
-		{126992, 0, 0, hnd_126992, &msg_126992[0]},
-		{126996, 1, 0, hnd_126996, &msg_126996[0]},
-		{129038, 1, 2, hnd_129038, &msg_129038[0]},
-		{129039, 1, 2, hnd_129039, &msg_129039[0]},
-		{129040, 1, 2, hnd_129040, &msg_129040[0]},
-		{129794, 1, 2, hnd_129794, &msg_129794[0]},
-		{129798, 1, 2, hnd_129798, &msg_129798[0]},
-		{129802, 1, 2, hnd_129802, &msg_129802[0]},
-		{129809, 1, 2, hnd_129809, &msg_129809[0]},
-		{129810, 1, 2, hnd_129810, &msg_129810[0]},
-		{0     , 0, 0, NULL,       &msg_error [0]}};
-/*@+usereleased@*/
-
 static int print_data(unsigned char *buffer, int len, PGN *pgn)
 {
     int   l1, l2, ptr;
@@ -437,6 +364,57 @@ gps_mask_t hnd_129810(unsigned char *bu, int len, PGN *pgn, struct gps_device_t 
     return(0);
 }
 
+/*@-usereleased@*/
+const char msg_059392[] = {"ISO  Acknowledgment"};
+const char msg_060928[] = {"ISO  Address Claim"};
+const char msg_126208[] = {"NMEA Command/Request/Acknowledge"};
+const char msg_126464[] = {"ISO  Transmit/Receive PGN List"};
+const char msg_126992[] = {"GNSS System Time"};
+const char msg_126996[] = {"ISO  Product Information"};
+const char msg_129025[] = {"GNSS Position Rapid Update"};
+const char msg_129026[] = {"GNSS COG and SOG Rapid Update"};
+const char msg_129029[] = {"GNSS Positition Data"};
+const char msg_129539[] = {"GNSS DOPs"};
+const char msg_129540[] = {"GNSS Satellites in View"};
+const char msg_129038[] = {"AIS  Class A Position Report"};
+const char msg_129039[] = {"AIS  Class B Position Report"};
+const char msg_129040[] = {"AIS  Class B Extended Position Report"};
+const char msg_129794[] = {"AIS  Class A Static and Voyage Related Data"};
+const char msg_129798[] = {"AIS  SAR Aircraft Position Report"};
+const char msg_129802[] = {"AIS  Safty Related Broadcast Message"};
+const char msg_129809[] = {"AIS  Class B CS Static Data Report, Part A"};
+const char msg_129810[] = {"AIS  Class B CS Static Data Report, Part B"};
+const char msg_error [] = {"**error**"};
+
+PGN gpspgn[] = {{ 59392, 0, 0, hnd_059392, &msg_059392[0]},
+		{ 60928, 0, 0, hnd_060928, &msg_060928[0]},
+		{126208, 0, 0, hnd_126208, &msg_126208[0]},
+		{126464, 1, 0, hnd_126464, &msg_126464[0]},
+		{126992, 0, 0, hnd_126992, &msg_126992[0]},
+		{126996, 1, 0, hnd_126996, &msg_126996[0]},
+		{129025, 0, 1, hnd_129025, &msg_129025[0]},
+		{129026, 0, 1, hnd_129026, &msg_129026[0]},
+		{129029, 1, 1, hnd_129029, &msg_129029[0]},
+		{129539, 0, 1, hnd_129539, &msg_129539[0]},
+		{129540, 1, 1, hnd_129540, &msg_129540[0]},
+		{0     , 0, 0, NULL,       &msg_error [0]}};
+
+PGN aispgn[] = {{ 59392, 0, 0, hnd_059392, &msg_059392[0]},
+		{ 60928, 0, 0, hnd_060928, &msg_060928[0]},
+		{126208, 0, 0, hnd_126208, &msg_126208[0]},
+		{126464, 1, 0, hnd_126464, &msg_126464[0]},
+		{126992, 0, 0, hnd_126992, &msg_126992[0]},
+		{126996, 1, 0, hnd_126996, &msg_126996[0]},
+		{129038, 1, 2, hnd_129038, &msg_129038[0]},
+		{129039, 1, 2, hnd_129039, &msg_129039[0]},
+		{129040, 1, 2, hnd_129040, &msg_129040[0]},
+		{129794, 1, 2, hnd_129794, &msg_129794[0]},
+		{129798, 1, 2, hnd_129798, &msg_129798[0]},
+		{129802, 1, 2, hnd_129802, &msg_129802[0]},
+		{129809, 1, 2, hnd_129809, &msg_129809[0]},
+		{129810, 1, 2, hnd_129810, &msg_129810[0]},
+		{0     , 0, 0, NULL,       &msg_error [0]}};
+/*@+usereleased@*/
 
 /*@-immediatetrans@*/
 static /*@null@*/ PGN *search_pgnlist(unsigned int pgn, PGN *pgnlist)
