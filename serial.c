1939f06f46b51ff34cb2b228005c4e794122722b
Eric S. Raymond <esr@thyrsus.com>
2012-06-20 09:23:25 -0400

    Make sure an invariant is preserved.

--- serial.c
+++ serial.c
@@ -517,7 +517,7 @@ ssize_t gpsd_write(struct gps_device_t * session, const char *buf, size_t len)
  * MAX_PACKET_LENGTH or we risk never syncing up at all.  Large values
  * will produce annoying startup lag.
  */
-#define SNIFF_RETRIES	256
+#define SNIFF_RETRIES	(MAX_PACKET_LENGTH + 128)
 
 bool gpsd_next_hunt_setting(struct gps_device_t * session)
 /* advance to the next hunt setting  */
