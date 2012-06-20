00b4c221bf2f864c269632930b374ec5c13a51af
Eric S. Raymond <esr@thyrsus.com>
2012-06-19 20:09:19 -0400

    splint gets confused by declarations after statements.

--- serial.c
+++ serial.c
@@ -360,8 +360,10 @@ int gpsd_serial_open(struct gps_device_t *session)
     /*@ +boolops +type @*/
 #ifdef HAVE_BLUEZ
     if (bachk(session->gpsdata.dev.path) == 0) {
-        session->gpsdata.gps_fd = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
         struct sockaddr_rc addr = { 0 };
+        session->gpsdata.gps_fd = socket(AF_BLUETOOTH,
+					 SOCK_STREAM,
+					 BTPROTO_RFCOMM);
         addr.rc_family = AF_BLUETOOTH;
         addr.rc_channel = (uint8_t) 1;
         str2ba(session->gpsdata.dev.path, &addr.rc_bdaddr);
