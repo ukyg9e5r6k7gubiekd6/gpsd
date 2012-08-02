7093057b3df79bdf49e65b15d9214e668ab20585
ukyg9e5r6k7gubiekd6 <ukyg9e5r6k7gubiekd6@mailinator.com>
2012-08-02 23:46:23 +1000

    sockaddr_t only needs to accomodate a sockaddr_in6 if IPV6_ENABLE.

--- sockaddr.h
+++ sockaddr.h
@@ -3,7 +3,9 @@
 typedef union sockaddr_u {
     struct sockaddr sa;
     struct sockaddr_in sa_in;
+#ifdef IPV6_ENABLE
     struct sockaddr_in6 sa_in6;
+#endif /* IPV6_ENABLE */
 } sockaddr_t;
 
 /* sockaddr.h ends here */
