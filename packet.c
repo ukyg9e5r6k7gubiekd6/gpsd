96621804887d8f800e535dbbaa6e85a482803fee
Eric S. Raymond <esr@thyrsus.com>
2013-10-17 04:50:21 -0400

    Narrow a variable scope. All regression tests pass.

--- packet.c
+++ packet.c
@@ -1356,20 +1356,21 @@ static void packet_accept(struct gps_packet_t *lexer, int packet_type)
 /* packet grab succeeded, move to output buffer */
 {
     size_t packetlen = lexer->inbufptr - lexer->inbuffer;
-    char scratchbuf[MAX_PACKET_LENGTH*2+1];
 
     if (packetlen < sizeof(lexer->outbuffer)) {
 	memcpy(lexer->outbuffer, lexer->inbuffer, packetlen);
 	lexer->outbuflen = packetlen;
 	lexer->outbuffer[packetlen] = '\0';
 	lexer->type = packet_type;
-	if (lexer->debug >= LOG_RAW+1)
+	if (lexer->debug >= LOG_RAW+1) {
+	    char scratchbuf[MAX_PACKET_LENGTH*2+1];
 	    gpsd_report(lexer->debug, LOG_RAW+1,
 			"Packet type %d accepted %zu = %s\n",
 			packet_type, packetlen,
 			gpsd_packetdump(scratchbuf,  sizeof(scratchbuf),
 					(char *)lexer->outbuffer,
 					lexer->outbuflen));
+	}
     } else {
 	gpsd_report(lexer->debug, LOG_ERROR,
 		    "Rejected too long packet type %d len %zu\n",
