39554efdf0416e35236ad3d23a3a893d90c68be6
Zbigniew Chyla <zbigniew.chyla@nsn.com>
2015-01-16 15:46:59 +0100

    Add new header for string functions.
    This change doesn't affect generated binary code.

---
+++ strfuncs.h
@@ -0,0 +1,12 @@
+/*
+ * strfuncs.h - string functions
+ *
+ * This software is distributed under a BSD-style license. See the
+ * file "COPYING" in the toop-level directory of the distribution for details.
+ */
+#ifndef _GPSD_STRFUNCS_H_
+#define _GPSD_STRFUNCS_H_
+
+#include <string.h>
+
+#endif /* _GPSD_STRFUNCS_H_ */
