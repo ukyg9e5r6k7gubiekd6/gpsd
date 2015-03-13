a85900b69f1b85f14a74945db12161573a315ca9
Jon Schlueter <jschlueter@navigationsolutions.com>
2015-03-13 10:34:18 -0400

    pep8 whitespace and is not None cleanup in gps/client.py

--- gps/client.py
+++ gps/client.py
@@ -6,9 +6,10 @@ import time, socket, sys, select, exceptions
 if sys.hexversion >= 0x2060000:
     import json			# For Python 2.6
 else:
-    import simplejson as json	# For Python 2.4 and 2.5
+    import simplejson as json 	# For Python 2.4 and 2.5
 
-GPSD_PORT="2947"
+GPSD_PORT = "2947"
+
 
 class json_error(exceptions.Exception):
     def __init__(self, data, explanation):
@@ -16,13 +17,14 @@ class json_error(exceptions.Exception):
         self.data = data
         self.explanation = explanation
 
+
 class gpscommon:
     "Isolate socket handling and buffering from the protocol interpretation."
     def __init__(self, host="127.0.0.1", port=GPSD_PORT, verbose=0):
         self.sock = None        # in case we blow up in connect
         self.linebuffer = ""
         self.verbose = verbose
-        if host != None:
+        if host is not None:
             self.connect(host, port)
 
     def connect(self, host, port):
@@ -35,11 +37,12 @@ class gpscommon:
         if not port and (host.find(':') == host.rfind(':')):
             i = host.rfind(':')
             if i >= 0:
-                host, port = host[:i], host[i+1:]
-            try: port = int(port)
+                host, port = host[:i], host[i + 1:]
+            try:
+                port = int(port)
             except ValueError:
                 raise socket.error, "nonnumeric port"
-        #if self.verbose > 0:
+        # if self.verbose > 0:
         #    print 'connect:', (host, port)
         msg = "getaddrinfo returns an empty list"
         self.sock = None
@@ -47,10 +50,10 @@ class gpscommon:
             af, socktype, proto, _canonname, sa = res
             try:
                 self.sock = socket.socket(af, socktype, proto)
-                #if self.debuglevel > 0: print 'connect:', (host, port)
+                # if self.debuglevel > 0: print 'connect:', (host, port)
                 self.sock.connect(sa)
             except socket.error, msg:
-                #if self.debuglevel > 0: print 'connect fail:', (host, port)
+                # if self.debuglevel > 0: print 'connect fail:', (host, port)
                 self.close()
                 continue
             break
@@ -121,17 +124,18 @@ class gpscommon:
             commands += "\n"
         self.sock.send(commands)
 
-WATCH_ENABLE	= 0x000001	# enable streaming
-WATCH_DISABLE	= 0x000002	# disable watching
-WATCH_JSON	= 0x000010	# JSON output
-WATCH_NMEA	= 0x000020	# output in NMEA
-WATCH_RARE	= 0x000040	# output of packets in hex
-WATCH_RAW	= 0x000080	# output of raw packets
-WATCH_SCALED	= 0x000100	# scale output to floats
-WATCH_TIMING	= 0x000200	# timing information
-WATCH_SPLIT24	= 0x001000	# split AIS Type 24s
-WATCH_PPS	= 0x002000	# enable PPS in raw/NMEA
-WATCH_DEVICE	= 0x000800	# watch specific device
+WATCH_ENABLE = 0x000001 	# enable streaming
+WATCH_DISABLE = 0x000002 	# disable watching
+WATCH_JSON = 0x000010   	# JSON output
+WATCH_NMEA = 0x000020   	# output in NMEA
+WATCH_RARE = 0x000040   	# output of packets in hex
+WATCH_RAW = 0x000080    	# output of raw packets
+WATCH_SCALED = 0x000100 	# scale output to floats
+WATCH_TIMING = 0x000200 	# timing information
+WATCH_SPLIT24 = 0x001000 	# split AIS Type 24s
+WATCH_PPS = 0x002000    	# enable PPS in raw/NMEA
+WATCH_DEVICE = 0x000800 	# watch specific device
+
 
 class gpsjson:
     "Basic JSON decoding."
@@ -168,7 +172,7 @@ class gpsjson:
                 arg += ',"split24":false'
             if flags & WATCH_PPS:
                 arg += ',"pps":false'
-        else: # flags & WATCH_ENABLE:
+        else:  # flags & WATCH_ENABLE:
             arg = '?WATCH={"enable":true'
             if flags & WATCH_JSON:
                 arg += ',"json":true'
@@ -190,22 +194,29 @@ class gpsjson:
                 arg += ',"device":"%s"' % devpath
         return self.send(arg + "}")
 
+
 class dictwrapper:
     "Wrapper that yields both class and dictionary behavior,"
     def __init__(self, ddict):
         self.__dict__ = ddict
+
     def get(self, k, d=None):
         return self.__dict__.get(k, d)
+
     def keys(self):
         return self.__dict__.keys()
+
     def __getitem__(self, key):
         "Emulate dictionary, for new-style interface."
         return self.__dict__[key]
+
     def __setitem__(self, key, val):
         "Emulate dictionary, for new-style interface."
         self.__dict__[key] = val
+
     def __contains__(self, key):
         return key in self.__dict__
+
     def __str__(self):
         return "<dictwrapper: " + str(self.__dict__) + ">"
     __repr__ = __str__
