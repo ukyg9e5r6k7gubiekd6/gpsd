1b0b9ed1efcf91cb57950ec3f8115cd49978cd33
Beat Bolli <dev@drbeat.li>
2016-02-20 13:21:05 +0100

    Eliminate a temporary variable.
    Iterating over a generator function is preferred and saves memory.
    
    Signed-off-by: Beat Bolli <dev@drbeat.li>
    Signed-off-by: Jon Schlueter <jschlueter@redhat.com>

--- devtools/identify_failing_build_options.py
+++ devtools/identify_failing_build_options.py
@@ -88,10 +88,9 @@ def main(starting_number_of_options=0):
     static_params += [key + '=off' for key in always_off]
 
     for i in range(starting_number_of_options, len(knobs)):
-        jj = itertools.combinations(knobs, i)
         print 'Testing at length {}'.format(i)
 
-        for row in list(jj):
+        for row in itertools.combinations(knobs, i):
             print row
             params = static_params + [key + '=on' for key in row]
 
