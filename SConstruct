ukyg9e5r6k7gubiekd6/gpsd

commit 79800e8bfd94cc11e66a9c0b7f550fb7c49db01e

purge yet another reference to libgpsd.pc
There was another missed reference under the insall phase

SConstruct
@@ -1414,7 +1414,7 @@ else:
                         python_progs_install,
                         python_egg_info_install]
 
-pc_install = [env.Install(installdir('pkgconfig'), x) for x in ("libgps.pc", "libgpsd.pc")]
+pc_install = [env.Install(installdir('pkgconfig'), x) for x in ("libgps.pc")]
 if qt_env:
     pc_install.append(qt_env.Install(installdir('pkgconfig'), 'Qgpsmm.pc'))
     pc_install.append(qt_env.Install(installdir('libdir'), 'libQgpsmm.prl'))

commit 6fc736ee17ae91dfb47e019e9aaa1d45ace96b0e

Fixes install breakage from commit 79800e8.
If the list of .pc files had been a list rather than a tuple, the
change would have worked.  But since it was a tuple, reducing it to
one item without including the comma as a "tuple flag" turned the
parens into simple parens, causing the "for" to iterate over the
characters of the single string instead of the strings.

Removing the now-superfluous tuple and iteration fixes it.

TESTED:
Verified that the previously failing install on Ubuntu works with this
fix.

Signed-off-by: Jon Schlueter <jon.schlueter@gmail.com>

SConstruct
@@ -1414,7 +1414,7 @@ else:
                         python_progs_install,
                         python_egg_info_install]
 
-pc_install = [env.Install(installdir('pkgconfig'), x) for x in ("libgps.pc")]
+pc_install = [env.Install(installdir('pkgconfig'), 'libgps.pc')]
 if qt_env:
     pc_install.append(qt_env.Install(installdir('pkgconfig'), 'Qgpsmm.pc'))
     pc_install.append(qt_env.Install(installdir('libdir'), 'libQgpsmm.prl'))