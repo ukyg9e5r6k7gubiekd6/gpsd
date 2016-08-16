31ece69babcaedbbd2f635c1dc2391025d5619b6
Gary E. Miller <gem@rellim.com>
2016-08-15 21:27:50 -0700

    cfmakeraw() needs _DEFAULT_SOURCE

--- serial.c
+++ serial.c
@@ -3,6 +3,9 @@
  * BSD terms apply: see the file COPYING in the distribution root for details.
  */
 
+/* cfmakeraw() needs _DEFAULT_SOURCE */
+#define _DEFAULT_SOURCE
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
