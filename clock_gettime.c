c1530269368964931868e2d0d3422cbaf7c85f76
Fred Wright <fw@fwright.net>
2017-01-05 17:27:21 -0800

    Fixes Mach port leak in OSX clock_gettime fallback.
    The commonly circulated code for nanosecond-resolution time of day in
    OSX has a bug.  The port obtained by mach_host_self() is a resource
    which needs to be freed after use.  This issue has been observed in
    connection with a different Mach port use in:
    
    	 https://codereview.chromium.org/279293002/
    
    It's not known how long one has to run gpsd in order to run afoul of
    this bug, or whether it has been observed in practice, but fixing it
    proactively is wise.  Note that this code disappears in macOS >=10.12,
    once native clock_gettime finally appeared.
    
    Also note that the host port is only needed to *obtain* the clock
    port, not to *use* the clock port, so it can be freed prior to
    actually reading the clock.
    
    TESTED:
    Ran "scons build-all check" on OSX 10.5-10.12.
    Ran gpsd for a few hours on OSX 10.9.5 (with one receiver).

--- clock_gettime.c
+++ clock_gettime.c
@@ -21,7 +21,9 @@ int clock_gettime(clockid_t clk_id UNUSED, struct timespec *ts)
 #ifdef __MACH__ // OS X and hurd do not have clock_gettime, use clock_get_time
     clock_serv_t cclock;
     mach_timespec_t mts;
-    host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+    mach_port_t mach_host = mach_host_self();
+    host_get_clock_service(mach_host, CALENDAR_CLOCK, &cclock);
+    mach_port_deallocate(mach_task_self(), mach_host);
     clock_get_time(cclock, &mts);
     mach_port_deallocate(mach_task_self(), cclock);
     ts->tv_sec = mts.tv_sec;
