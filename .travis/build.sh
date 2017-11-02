ukyg9e5r6k7gubiekd6/gpsd

commit 2f6e5b674d63942d39d647dbe397f56824f2727d

travis.ci: add qt_versioned to scons options.

.travis/build.sh
@@ -17,6 +17,13 @@ SCONSOPTS="${SCONSOPTS} libdir=/usr/lib/${DEB_HOST_MULTIARCH}"
 SCONSOPTS="${SCONSOPTS} gpsd_user=gpsd"
 SCONSOPTS="${SCONSOPTS} gpsd_group=dialout"
 SCONSOPTS="${SCONSOPTS} debug=yes"
+SCONSOPTS="${SCONSOPTS} qt=yes"
+
+if dpkg -s qtbase5-dev 1>/dev/null 2>&1; then
+    SCONSOPTS="${SCONSOPTS} qt_versioned=5"
+else
+    SCONSOPTS="${SCONSOPTS} qt_versioned=4"
+fi
 
 export SCONSOPTS
 

commit 7e7c5478fbda652e2c2c8adaae62e11aed12d78b

Fixes broken Travis clang++ workaround.
Commit 66df9308 disabled C++ builds on Travis when using clang++, in
order to get around an unresolved issue with __float128.  Hovever,
commit 2f6e5b67 broke the workround by adding an explicit qt=yes to
SCONSOPTS.  This wasn't immediately noticed, since that commit also
broke the use of Qt4 altogether, and there is no case that builds with
Qt5 and clang++.  Fixing the Qt4 bug in commit 2c01c135 then
reintroduced the old clang++ bug.

This change removes the troublesome explicit qt=yes.

TESTED:
Will be once this change is pushed and picked up by Travis.  It has no
effect outside Travis.

.travis/build.sh
@@ -17,7 +17,7 @@ SCONSOPTS="${SCONSOPTS} libdir=/usr/lib/${DEB_HOST_MULTIARCH}"
 SCONSOPTS="${SCONSOPTS} gpsd_user=gpsd"
 SCONSOPTS="${SCONSOPTS} gpsd_group=dialout"
 SCONSOPTS="${SCONSOPTS} debug=yes"
-SCONSOPTS="${SCONSOPTS} qt=yes"
+### SCONSOPTS="${SCONSOPTS} qt=yes"  # The default qt=yes must be overridable
 
 if dpkg -s qtbase5-dev 1>/dev/null 2>&1; then
     SCONSOPTS="${SCONSOPTS} qt_versioned=5"