57e3efff5cdcd59e7490ea2704f4348b6f485af0
Gary E. Miller <gem@rellim.com>
2017-12-15 18:48:45 -0800

    Support format strings "RTCM 3", fix bug #52277

--- net_ntrip.c
+++ net_ntrip.c
@@ -89,7 +89,8 @@ static void ntrip_str_parse(char *str, size_t len,
 	/* required for the SAPOS derver in Gemany, confirmed as RTCM2.3 */
 	else if (strcasecmp("RTCM1_", s) == 0)
 	    hold->format = fmt_rtcm2_3;
-	else if (strcasecmp("RTCM 3.0", s) == 0)
+	else if ((strcasecmp("RTCM 3", s) == 0) ||
+	         (strcasecmp("RTCM 3.0", s) == 0))
 	    hold->format = fmt_rtcm3_0;
 	else if (strcasecmp("RTCM 3.1", s) == 0)
 	    hold->format = fmt_rtcm3_1;
