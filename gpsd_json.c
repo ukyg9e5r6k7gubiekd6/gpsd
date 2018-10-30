7f0f0bf02ad171506d2f777540559ce6952a4d89
Gary E. Miller <gem@rellim.com>
2018-10-29 18:28:19 -0700

    gpsd_json: Add freqid and obs (obs_code) to RAW message.

--- gpsd_json.c
+++ gpsd_json.c
@@ -742,9 +742,12 @@ void json_raw_dump(const struct gps_data_t *gpsdata,
         if (0 == gpsdata->raw.meas[i].svid) {
             continue;
         }
-        str_appendf(reply, replylen, "{\"gnssid\":%d,\"svid\":%d,\"snr\":%d",
+        str_appendf(reply, replylen,
+                    "{\"gnssid\":%d,\"svid\":%d,\"snr\":%d,\"freqid\":%d,"
+                    "\"obs\":\"%3s\"",
                     gpsdata->raw.meas[i].gnssid, gpsdata->raw.meas[i].svid,
-                    gpsdata->raw.meas[i].snr);
+                    gpsdata->raw.meas[i].snr, gpsdata->raw.meas[i].freqid,
+                    gpsdata->raw.meas[i].obs_code);
         comma = true;
 
         if (0 != isfinite(gpsdata->raw.meas[i].carrierphase)) {
