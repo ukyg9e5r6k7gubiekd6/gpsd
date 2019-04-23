ce996643fe527e4448774a029f9629f152900642
Gary E. Miller <gem@rellim.com>
2019-04-23 14:40:08 -0700

    libgpsd_core.c: Improve error message a tad.

--- libgpsd_core.c
+++ libgpsd_core.c
@@ -1548,10 +1548,12 @@ gps_mask_t gpsd_poll(struct gps_device_t *session)
 	if ((session->gpsdata.set & TIME_SET) != 0) {
 	    if (session->newdata.time > time(NULL) + (60 * 60 * 24 * 365))
 		gpsd_log(&session->context->errout, LOG_WARN,
-			 "date more than a year in the future!\n");
+			 "date (%,3f) more than a year in the future!\n",
+			 session->newdata.time);
 	    else if (session->newdata.time < 0)
 		gpsd_log(&session->context->errout, LOG_ERROR,
-			 "date is negative!\n");
+			 "date (%.3f) is negative!\n",
+			 session->newdata.time);
 	}
 
 	return session->gpsdata.set;
