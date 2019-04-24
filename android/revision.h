2e4ca963280acc6a7414a6d9913d1eef7f09032c
Adam Serbinski <adam@serbinski.com>
2019-04-24 09:36:46 -0400

    Android: Build gpsd
    Signed-off-by: Adam Serbinski <adam@serbinski.com>
    Signed-off-by: Gary E. Miller <gem@rellim.com>

---
+++ android/revision.h
@@ -0,0 +1,1 @@
+#define REVISION "android-no-rev"
---
+++ android/timebase.h
@@ -0,0 +1,9 @@
+/*
+ * Constants used for GPS time detection and rollover correction.
+ *
+ * Correct for week beginning 2019-04-18T00:00:00
+ */
+#define BUILD_CENTURY	2000
+#define BUILD_WEEK	2                   # Assumes 10-bit week counter
+#define BUILD_LEAPSECONDS	19
+#define BUILD_ROLLOVERS	2         # Assumes 10-bit week counter
