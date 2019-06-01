229b8bf20d91b349557cd6340a82c36587b925d4
Sanjeev Gupta <ghane0@gmail.com>
2019-06-01 11:45:47 +0800

    Update references to http to https wor w3.org domain
    Manually checked that each converted URL works

--- gpxlogger.c
+++ gpxlogger.c
@@ -45,7 +45,7 @@ static void print_gpx_header(void)
 
     (void)fprintf(logfile,"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
     (void)fprintf(logfile,"<gpx version=\"1.1\" creator=\"GPSD %s - %s\"\n", VERSION, GPSD_URL);
-    (void)fprintf(logfile,"        xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n");
+    (void)fprintf(logfile,"        xmlns:xsi=\"https://www.w3.org/2001/XMLSchema-instance\"\n");
     (void)fprintf(logfile,"        xmlns=\"http://www.topografix.com/GPX/1/1\"\n");
     (void)fprintf(logfile,"        xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1\n");
     (void)fprintf(logfile,"        http://www.topografix.com/GPX/1/1/gpx.xsd\">\n");
