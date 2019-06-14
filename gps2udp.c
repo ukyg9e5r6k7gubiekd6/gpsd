d36c45c20996a89d62bfcf5365ec5f412f3a7c81
Gary E. Miller <gem@rellim.com>
2019-06-14 16:17:56 -0700

    gps2udp: Note some include files.

--- gps2udp.c
+++ gps2udp.c
@@ -18,12 +18,12 @@
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <netdb.h>
+#include <netdb.h>        /* for gethostbyname() */
 #include <netinet/in.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h>      /* for strlcpy(), strsep(), etc. */
 #include <strings.h>
 #include <sys/select.h>
 #include <sys/socket.h>
