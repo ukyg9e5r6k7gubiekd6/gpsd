aca96721b0db3a342eedd797cbe11a57ba321399
Gary E. Miller <gem@rellim.com>
2019-07-02 19:11:01 -0700

    os_compat.c: Remove long unused strlcpy() code.

--- os_compat.c
+++ os_compat.c
@@ -293,49 +293,6 @@ size_t strlcpy(char *dst, const char *src, size_t siz)
     return len;
 }
 
-#ifdef __UNUSED__
-/*	$OpenBSD: strlcpy.c,v 1.11 2006/05/05 15:27:38 millert Exp $	*/
-
-/*
- * Copyright (c) 1998 Todd C. Miller <Todd.Miller@courtesan.com>
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
-size_t strlcpy(char *dst, const char *src, size_t siz)
-{
-    char *d = dst;
-    const char *s = src;
-    size_t n = siz;
-
-    /* Copy as many bytes as will fit */
-    if (n != 0) {
-	while (--n != 0) {
-	    if ((*d++ = *s++) == '\0')
-		break;
-	}
-    }
-
-    /* Not enough room in dst, add NUL and traverse rest of src */
-    if (n == 0) {
-	if (siz != 0)
-	    *d = '\0';		/* NUL-terminate dst */
-	while (*s++ != '\0')
-	    continue;
-    }
-
-    return ((size_t) (s - src - 1));	/* count does not include NUL */
-}
-#endif /* __UNUSED__ */
 #endif /* !HAVE_STRLCPY */
 
 /* End of strlcat()/strlcpy() section */
