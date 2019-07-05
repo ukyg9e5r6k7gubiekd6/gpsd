6c82fee0f1cd831a06a6fc07c1771c27f9528c8a
Gary E. Miller <gem@rellim.com>
2019-07-05 12:16:27 -0700

    gpsd_json.c: Put new NED and geoid_sep in TPV JSON.

--- gpsd_json.c
+++ gpsd_json.c
@@ -218,6 +218,28 @@ void json_tpv_dump(const struct gps_device_t *session,
 	    if (0 != isfinite(gpsdata->fix.ecef.vAcc))
 		str_appendf(reply, replylen, "\"ecefvAcc\":%.2f,",
 			    gpsdata->fix.ecef.vAcc);
+	    /* NED is in meters, so %.3f is millimeter resolution */
+	    if (0 != isfinite(gpsdata->fix.NED.relPosN))
+		str_appendf(reply, replylen, "\"relN\":%.3f,",
+			    gpsdata->fix.NED.relPosN);
+	    if (0 != isfinite(gpsdata->fix.NED.relPosE))
+		str_appendf(reply, replylen, "\"relE\":%.3f,",
+			    gpsdata->fix.NED.relPosE);
+	    if (0 != isfinite(gpsdata->fix.NED.relPosD))
+		str_appendf(reply, replylen, "\"relD\":%.3f,",
+			    gpsdata->fix.NED.relPosD);
+	    if (0 != isfinite(gpsdata->fix.NED.velN))
+		str_appendf(reply, replylen, "\"velN\":%.3f,",
+			    gpsdata->fix.NED.velN);
+	    if (0 != isfinite(gpsdata->fix.NED.velE))
+		str_appendf(reply, replylen, "\"velE\":%.3f,",
+			    gpsdata->fix.NED.velE);
+	    if (0 != isfinite(gpsdata->fix.NED.velD))
+		str_appendf(reply, replylen, "\"velD\":%.3f,",
+			    gpsdata->fix.NED.velD);
+	    if (0 != isfinite(gpsdata->fix.geoid_sep))
+		str_appendf(reply, replylen, "\"geoidSep\":%.3f,",
+			    gpsdata->fix.geoid_sep);
         }
 #ifdef TIMING_ENABLE
 	if (policy->timing) {
