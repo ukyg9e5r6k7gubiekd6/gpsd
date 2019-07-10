784ff565230a3fa8dd26aa6b17401a407fe1f782
Gary E. Miller <gem@rellim.com>
2019-07-09 20:26:04 -0700

    driver_italk: Move gpsdata.separation to newdata.geoid_sep
    Improved regression with geoidSep.

--- driver_italk.c
+++ driver_italk.c
@@ -73,7 +73,7 @@ static gps_mask_t decode_itk_navfix(struct gps_device_t *session,
     session->newdata.ecef.vx = (double)(getles32(buf, 7 + 186) / 1000.0);
     session->newdata.ecef.vy = (double)(getles32(buf, 7 + 190) / 1000.0);
     session->newdata.ecef.vz = (double)(getles32(buf, 7 + 194) / 1000.0);
-    ecef_to_wgs84fix(&session->newdata, &session->gpsdata.separation,
+    ecef_to_wgs84fix(&session->newdata, &session->newdata.geoid_sep,
 		     session->newdata.ecef.x, session->newdata.ecef.y,
 		     session->newdata.ecef.z, session->newdata.ecef.vx,
 		     session->newdata.ecef.vy, session->newdata.ecef.vz);
