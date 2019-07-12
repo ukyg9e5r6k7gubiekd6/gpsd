44cb39b4546d6cc021cd54aa61c9a56ee5a3039f
Gary E. Miller <gem@rellim.com>
2019-07-12 12:35:34 -0700

    geoid.c: remove redundant climb calculation.
    This was over writing another climb number in one regression.

--- geoid.c
+++ geoid.c
@@ -103,12 +103,12 @@ gps_mask_t ecef_to_wgs84fix(struct gps_fix_t *fix, double *separation,
 		            double x, double y, double z,
 		            double vx, double vy, double vz)
 {
-    double lambda, phi, p, theta, n, h, vnorth, veast, heading;
+    double lambda, phi, p, theta, n, h, vnorth, veast, vup, heading;
     const double a = WGS84A;	/* equatorial radius */
     const double b = WGS84B;	/* polar radius */
     const double e2 = (a * a - b * b) / (a * a);
     const double e_2 = (a * a - b * b) / (b * b);
-    gps_mask_t mask = (LATLON_SET | ALTITUDE_SET | CLIMB_SET | TRACK_SET |
+    gps_mask_t mask = (LATLON_SET | ALTITUDE_SET | TRACK_SET |
                        SPEED_SET);
 
     /* geodetic location */
@@ -130,24 +130,17 @@ gps_mask_t ecef_to_wgs84fix(struct gps_fix_t *fix, double *separation,
 	     vz * cos(phi);
     veast = -vx * sin(lambda) + vy * cos(lambda);
 
-    fix->climb =
-	vx * cos(phi) * cos(lambda) + vy * cos(phi) * sin(lambda) +
-	vz * sin(phi);
+    vup = vx * cos(phi) * cos(lambda) + vy * cos(phi) * sin(lambda) +
+	  vz * sin(phi);
 
     /* save velNED */
     fix->NED.velN = vnorth;
     fix->NED.velE = veast;
-    fix->NED.velD = -fix->climb;
+    fix->NED.velD = -vup;
     mask |= VNED_SET;
 
     /* FIXME: after velNED is saved, let gpsd_error_model() do the
      * sanity checks and speed/track */
-
-    /* sanity check the climb, 10,000 m/s should be a nice max */
-    if ( 9999.9 < fix->climb )
-	fix->climb = NAN;
-    else if ( -9999.9 > fix->climb )
-	fix->climb = NAN;
 
     fix->speed = hypot(vnorth, veast);
     /* sanity check the speed, 10,000 m/s should be a nice max */
