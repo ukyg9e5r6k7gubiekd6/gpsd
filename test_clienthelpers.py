dc4eb646b981a2614adb77892ae22ba22802d985
Gary E. Miller <gem@rellim.com>
2019-07-17 15:04:40 -0700

    test_clienthelpers.py: Commented on correct separations.
    One is wrong by over 15 meters!

--- test_clienthelpers.py
+++ test_clienthelpers.py
@@ -55,18 +55,31 @@ test3 = [
     # https://geographiclib.sourceforge.io/cgi-bin/GeoidEval
     # what MSL is this?  EGM208, EGM96, EGM84, or??
     # Seems closest to EGM2008, but still off by a meter.
+
+    # Easter Island: EGM2008 -3.8178, EGM96 -4.9979, EGM84 -5.1408
     (-27.1127, -109.3497, -5.4363, "Easter Island"),
+    # Kalahar: EGM2008 27.6676, EGM96 27.6659, EGM84 27.1081
     (-25.5920, 21.0937, 27.3370,  "Kalahari Desert"),
+    # Greenland: EGM2008 40.3981, EGM96 40.5912, EGM84 41.7056
     (71.7069, -42.6043, 41.9313, "Greenland"),
+    # Kuk Swamp: EGM2008 82.3395, EGM96 82.6719, EGM84 79.2188
     (-5.7837, 144.3317, 70.5250, "Kuk Swamp PG"),
+    # KBDN: EGM2008 -20.3509, EGM96 -19.8008, EGM84 -18.5562
     (44.094556, -121.200222, -20.0215, "Bend Airport, OR (KBDN)"),
+    # PANC: EGM2008 7.6508, EGM96 7.8563, EGM84 8.0838
     (61.171333, -149.991164, 8.7136, "Anchorage Airport, AK (PANC)"),
+    # KDEN: EGM2008 -18.1941, EGM96 -18.4209, EGM84 -15.9555
     (39.861666, -104.673166, -21.3813, "Denver Airport, CO (KDEN)"),
+    # KDEN: EGM2008 46.4499, EGM96 46.3061, EGM84 47.7620
     (51.46970, -0.45943, 47.5378, "London Heathrow Airport, UK (LHR)"),
+    # SCPE: EGM2008 37.9575, EGM96 39.3446, EGM84 46.6672
     (-22.92170, -68.158401, 31.3072,
      "San Pedro de Atacama Airport, CL (SCPE)"),
+    # SIN: EGM2008 8.6453, EGM96 8.3503, EGM84 8.2509
     (1.350190, 103.994003, 5.4259, "Singapore Changi Airport, SG (SIN)"),
+    # UURB: EGM2008 13.6322, EGM96 13.6448, EGM84 13.1280
     (55.617199, 38.06000, 14.9197, "Moscow Bykovo Airport, RU (UUBB)"),
+    # SYD: EGM2008 22.0915, EGM96 22.2038, EGM84 21.7914
     (-33.946098, 151.177002, 19.5850, "Sydney Airport, AU (SYD)"),
     ]
 
