01cbfd99e614aeb788d3f972acb60e3c92bc7351
James Browning <JamesB.fe80@gmail.com>
2019-07-21 03:58:11 -0700

    Strip out some tabs to vim shim

--- tests/test_gpsmm.cpp
+++ tests/test_gpsmm.cpp
@@ -39,78 +39,78 @@ static void libgps_dump_state(struct gps_data_t *collect)
     /* no need to dump the entire state, this is a sanity check */
 #ifndef USE_QT
     (void)fprintf(stdout, "flags: (0x%04x) %s\n",
-		  (unsigned int)collect->set, gps_maskdump(collect->set));
+                  (unsigned int)collect->set, gps_maskdump(collect->set));
 #endif
     if (collect->set & ONLINE_SET)
-	(void)fprintf(stdout, "ONLINE: %lf\n", collect->online);
+        (void)fprintf(stdout, "ONLINE: %lf\n", collect->online);
     if (collect->set & TIME_SET)
-	(void)fprintf(stdout, "TIME: %lf\n", collect->fix.time);
+        (void)fprintf(stdout, "TIME: %lf\n", collect->fix.time);
     if (collect->set & LATLON_SET)
-	(void)fprintf(stdout, "LATLON: lat/lon: %lf %lf\n",
-		      collect->fix.latitude, collect->fix.longitude);
+        (void)fprintf(stdout, "LATLON: lat/lon: %lf %lf\n",
+                      collect->fix.latitude, collect->fix.longitude);
     if (collect->set & ALTITUDE_SET)
-	(void)fprintf(stdout, "ALTITUDE: altitude: %lf  U: climb: %lf\n",
-		      collect->fix.altitude, collect->fix.climb);
+        (void)fprintf(stdout, "ALTITUDE: altitude: %lf  U: climb: %lf\n",
+                      collect->fix.altitude, collect->fix.climb);
     if (collect->set & SPEED_SET)
-	(void)fprintf(stdout, "SPEED: %lf\n", collect->fix.speed);
+        (void)fprintf(stdout, "SPEED: %lf\n", collect->fix.speed);
     if (collect->set & TRACK_SET)
-	(void)fprintf(stdout, "TRACK: track: %lf\n", collect->fix.track);
+        (void)fprintf(stdout, "TRACK: track: %lf\n", collect->fix.track);
     if (collect->set & CLIMB_SET)
-	(void)fprintf(stdout, "CLIMB: climb: %lf\n", collect->fix.climb);
+        (void)fprintf(stdout, "CLIMB: climb: %lf\n", collect->fix.climb);
     if (collect->set & STATUS_SET)
-	(void)fprintf(stdout, "STATUS: status: %d\n", collect->status);
+        (void)fprintf(stdout, "STATUS: status: %d\n", collect->status);
     if (collect->set & MODE_SET)
-	(void)fprintf(stdout, "MODE: mode: %d\n", collect->fix.mode);
+        (void)fprintf(stdout, "MODE: mode: %d\n", collect->fix.mode);
     if (collect->set & DOP_SET)
-	(void)fprintf(stdout,
-		      "DOP: satellites %d, pdop=%lf, hdop=%lf, vdop=%lf\n",
-		      collect->satellites_used, collect->dop.pdop,
-		      collect->dop.hdop, collect->dop.vdop);
+        (void)fprintf(stdout,
+                      "DOP: satellites %d, pdop=%lf, hdop=%lf, vdop=%lf\n",
+                      collect->satellites_used, collect->dop.pdop,
+                      collect->dop.hdop, collect->dop.vdop);
     if (collect->set & VERSION_SET)
-	(void)fprintf(stdout, "VERSION: release=%s rev=%s proto=%d.%d\n",
-		      collect->version.release,
-		      collect->version.rev,
-		      collect->version.proto_major,
-		      collect->version.proto_minor);
+        (void)fprintf(stdout, "VERSION: release=%s rev=%s proto=%d.%d\n",
+                      collect->version.release,
+                      collect->version.rev,
+                      collect->version.proto_major,
+                      collect->version.proto_minor);
     if (collect->set & POLICY_SET)
-	(void)fprintf(stdout,
-		      "POLICY: watcher=%s nmea=%s raw=%d scaled=%s timing=%s, devpath=%s\n",
-		      collect->policy.watcher ? "true" : "false",
-		      collect->policy.nmea ? "true" : "false",
-		      collect->policy.raw,
-		      collect->policy.scaled ? "true" : "false",
-		      collect->policy.timing ? "true" : "false",
-		      collect->policy.devpath);
+        (void)fprintf(stdout,
+                      "POLICY: watcher=%s nmea=%s raw=%d scaled=%s timing=%s, devpath=%s\n",
+                      collect->policy.watcher ? "true" : "false",
+                      collect->policy.nmea ? "true" : "false",
+                      collect->policy.raw,
+                      collect->policy.scaled ? "true" : "false",
+                      collect->policy.timing ? "true" : "false",
+                      collect->policy.devpath);
     if (collect->set & SATELLITE_SET) {
-	int i;
+        int i;
 
-	(void)fprintf(stdout, "SKY: satellites in view: %d\n",
-		      collect->satellites_visible);
-	for (i = 0; i < collect->satellites_visible; i++) {
-	    (void)fprintf(stdout, "    %3d", collect->skyview[i].PRN);
-	    printfinite(collect->skyview[i].elevation);
-	    printfinite(collect->skyview[i].azimuth);
-	    printfinite(collect->skyview[i].ss);
-	    (void)fprintf(stdout, collect->skyview[i].used ? " Y\n" : " N\n");
-	}
+        (void)fprintf(stdout, "SKY: satellites in view: %d\n",
+                      collect->satellites_visible);
+        for (i = 0; i < collect->satellites_visible; i++) {
+            (void)fprintf(stdout, "    %3d", collect->skyview[i].PRN);
+            printfinite(collect->skyview[i].elevation);
+            printfinite(collect->skyview[i].azimuth);
+            printfinite(collect->skyview[i].ss);
+            (void)fprintf(stdout, collect->skyview[i].used ? " Y\n" : " N\n");
+        }
     }
     if (collect->set & DEVICE_SET)
-	(void)fprintf(stdout, "DEVICE: Device is '%s', driver is '%s'\n",
-		      collect->dev.path, collect->dev.driver);
+        (void)fprintf(stdout, "DEVICE: Device is '%s', driver is '%s'\n",
+                      collect->dev.path, collect->dev.driver);
 #ifdef OLDSTYLE_ENABLE
     if (collect->set & DEVICEID_SET)
-	(void)fprintf(stdout, "GPSD ID is %s\n", collect->dev.subtype);
+        (void)fprintf(stdout, "GPSD ID is %s\n", collect->dev.subtype);
 #endif /* OLDSTYLE_ENABLE */
     if (collect->set & DEVICELIST_SET) {
-	int i;
-	(void)fprintf(stdout, "DEVICELIST:%d devices:\n",
-		      collect->devices.ndevices);
-	for (i = 0; i < collect->devices.ndevices; i++) {
-	    (void)fprintf(stdout, "%d: path='%s' driver='%s'\n",
-			  collect->devices.ndevices,
-			  collect->devices.list[i].path,
-			  collect->devices.list[i].driver);
-	}
+        int i;
+        (void)fprintf(stdout, "DEVICELIST:%d devices:\n",
+                      collect->devices.ndevices);
+        for (i = 0; i < collect->devices.ndevices; i++) {
+            (void)fprintf(stdout, "%d: path='%s' driver='%s'\n",
+                          collect->devices.ndevices,
+                          collect->devices.list[i].path,
+                          collect->devices.list[i].driver);
+        }
     }
 }
 
@@ -141,15 +141,15 @@ int main(int argc, char *argv[])
     struct fixsource_t source;
     /* Grok the server, port, and device. */
     if (optind < argc) {
-	gpsd_source_spec(argv[optind], &source);
+        gpsd_source_spec(argv[optind], &source);
     } else
-	gpsd_source_spec(NULL, &source);
+        gpsd_source_spec(NULL, &source);
 
     //gpsmm gps_rec("localhost", DEFAULT_GPSD_PORT);
     gpsmm gps_rec(source.server, source.port);
 
     if ( !((std::string)source.server == (std::string)GPSD_SHARED_MEMORY ||
-	   (std::string)source.server == (std::string)GPSD_DBUS_EXPORT) ) {
+           (std::string)source.server == (std::string)GPSD_DBUS_EXPORT) ) {
         if (gps_rec.stream(WATCH_ENABLE|WATCH_JSON) == NULL) {
             cerr << "No GPSD running.\n";
             return 1;
@@ -161,17 +161,17 @@ int main(int argc, char *argv[])
     // So with the 5 second wait & a 4 byte uint - this equates to ~680 years
     //  - long enough for a test program :)
     for (uint ll = 0; ll < looper; ll++) {
-	struct gps_data_t* newdata;
+        struct gps_data_t* newdata;
 
-	if (!gps_rec.waiting(5000000))
-	  continue;
+        if (!gps_rec.waiting(5000000))
+          continue;
 
-	if ((newdata = gps_rec.read()) == NULL) {
-	    cerr << "Read error.\n";
-	    return 1;
-	} else {
-	    libgps_dump_state(newdata);
-	}
+        if ((newdata = gps_rec.read()) == NULL) {
+            cerr << "Read error.\n";
+            return 1;
+        } else {
+            libgps_dump_state(newdata);
+        }
     }
 
     cout << "Exiting\n";
