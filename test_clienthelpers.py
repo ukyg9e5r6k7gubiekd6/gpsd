3edbaa0f5c26117a450a3de47dbc147488d9ca77
Gary E. Miller <gem@rellim.com>
2019-07-22 14:25:30 -0700

    geoid.c: Use new geoid_delta of 5.5 degree data.
    Modest altMSL improvements.  Still some 12 m errors.

--- test_clienthelpers.py
+++ test_clienthelpers.py
@@ -59,30 +59,32 @@ test3 = [
     # Seems closest to EGM2008, but still off by a meter.
 
     # Easter Island: EGM2008 -3.8178, EGM96 -4.9979, EGM84 -5.1408
-    (-27.1127, -109.3497, -6.05, "Easter Island"),
+    (-27.1127, -109.3497, -5.75, "Easter Island"),
     # Kalahar: EGM2008 27.6676, EGM96 27.6659, EGM84 27.1081
-    (-25.5920, 21.0937, 27.31,  "Kalahari Desert"),
+    (-25.5920, 21.0937, 27.62,  "Kalahari Desert"),
     # Greenland: EGM2008 40.3981, EGM96 40.5912, EGM84 41.7056
-    (71.7069, -42.6043, 39.48, "Greenland"),
+    (71.7069, -42.6043, 40.11, "Greenland"),
     # Kuk Swamp: EGM2008 82.3395, EGM96 82.6719, EGM84 79.2188
-    (-5.7837, 144.3317, 70.65, "Kuk Swamp PG"),
+    # seems to be over a gravitational anomaly
+    (-5.7837, 144.3317, 71.08, "Kuk Swamp PG"),
     # KBDN: EGM2008 -20.3509, EGM96 -19.8008, EGM84 -18.5562
-    (44.094556, -121.200222, -21.23, "Bend Airport, OR (KBDN)"),
+    (44.094556, -121.200222, -21.95, "Bend Airport, OR (KBDN)"),
     # PANC: EGM2008 7.6508, EGM96 7.8563, EGM84 8.0838
-    (61.171333, -149.991164, 12.55, "Anchorage Airport, AK (PANC)"),
+    (61.171333, -149.991164, 13.54, "Anchorage Airport, AK (PANC)"),
     # KDEN: EGM2008 -18.1941, EGM96 -18.4209, EGM84 -15.9555
-    (39.861666, -104.673166, -21.64, "Denver Airport, CO (KDEN)"),
+    (39.861666, -104.673166, -18.15, "Denver Airport, CO (KDEN)"),
     # KDEN: EGM2008 46.4499, EGM96 46.3061, EGM84 47.7620
-    (51.46970, -0.45943, 46.06, "London Heathrow Airport, UK (LHR)"),
+    (51.46970, -0.45943, 46.26, "London Heathrow Airport, UK (LHR)"),
     # SCPE: EGM2008 37.9575, EGM96 39.3446, EGM84 46.6672
-    (-22.92170, -68.158401, 32.37, "San Pedro de Atacama Airport, CL (SCPE)"),
+    (-22.92170, -68.158401, 35.46, "San Pedro de Atacama Airport, CL (SCPE)"),
     # SIN: EGM2008 8.6453, EGM96 8.3503, EGM84 8.2509
-    (1.350190, 103.994003, 6.55, "Singapore Changi Airport, SG (SIN)"),
+    (1.350190, 103.994003, 7.51, "Singapore Changi Airport, SG (SIN)"),
     # UURB: EGM2008 13.6322, EGM96 13.6448, EGM84 13.1280
-    (55.617199, 38.06000, 13.04, "Moscow Bykovo Airport, RU (UUBB)"),
+    (55.617199, 38.06000, 13.22, "Moscow Bykovo Airport, RU (UUBB)"),
     # SYD: EGM2008 22.0915, EGM96 22.2038, EGM84 21.7914
-    (-33.946098, 151.177002, 18.96, "Sydney Airport, AU (SYD)"),
-    # some 10x10 points
+    (-33.946098, 151.177002, 21.89, "Sydney Airport, AU (SYD)"),
+
+    # some 10x10 points, s/b exact EGM2008, +/- rounding
     # North Poll: EGM2008 14.8980, EGM96 13.6050, EGM84 13.0980
     (90, 0, 14.90, "North Poll"),
     # Doyle: EGM2008 -23.3366, EGM96 -23.3278, EGM84 -21.1672
@@ -91,11 +93,22 @@ test3 = [
     (0, 0, 17.23, "Equator 0W"),
     # South Poll: EGM2008 -30.1500, EGM96 -29.5350, EGM84 -29.7120
     (-90, 0, -30.15, "South Poll"),
+
+    # some 5x5 points, s/b exact EGM2008, +/- rounding
+    # -5, -5: EGM2008 17.1068, EGM96 16.8362, EGM84 17.5916
+    (-5, -5, 17.11, "-5, -5"),
+    # -5, 5: EGM2008 9.3988, EGM96 9.2399, EGM84 9.7948
+    (-5, 5, 9.40, "-5, 5"),
+    # 5, 5:  EGM2008 21.2609, EGM96 20.8917, EGM84 20.3509
+    (5, 5, 21.26, "5, 5"),
+    # 5, -5: EGM2008 25.7668, EGM96 25.6144, EGM84 25.1224
+    (5, -5, 25.77, "5, 5"),
+
     # test data for some former corners in the code
-    # 0, -78.*: EGM2008 26.8978, EGM96 25.3457, EGM84 26.1507
-    (0, -78.452222, 14.46, "Equatorial Sign Bolivia"),
-    # 51.*, 0: EGM2008 45.8961, EGM96 45.7976, EGM84 47.2468
-    (51.4778067, 0, 45.48, "Lawn Greenwich Observatory UK"),
+    # 0, -78.452222: EGM2008 26.8978, EGM96 25.3457, EGM84 26.1507
+    (0, -78.452222, 15.98, "Equatorial Sign Bolivia"),
+    # 51.4778067, 0: EGM2008 45.8961, EGM96 45.7976, EGM84 47.2468
+    (51.4778067, 0, 45.46, "Lawn Greenwich Observatory UK"),
     ]
 
 test4 = [
