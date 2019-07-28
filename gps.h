276bfc0d1208dbb0fbd3df3526db166d7695bd91
Gary E. Miller <gem@rellim.com>
2019-07-27 19:38:36 -0700

    gps.h: Not all chars are signed, but frqid must be signed.
    No functional change on most architectures.

--- gps.h
+++ gps.h
@@ -1922,7 +1922,7 @@ struct satellite_t {
      * GLONASS:  1 = L1 OF, 3 = L2 OF
      */
     unsigned char sigid;
-    char freqid;       /* The GLONASS (Only) frequency, 0 - 13 */
+    signed char freqid;         /* The GLONASS (Only) frequency, 0 - 13 */
     unsigned char health;       /* 0 = unknown, 1 = helthy, 2 = unhealthy */
 #define SAT_HEALTH_UNK 0
 #define SAT_HEALTH_OK 1
