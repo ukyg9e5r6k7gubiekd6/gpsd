49ccfbab4f5b97aaa328ea13e60890577d4ca32f
Bernd Zeimetz <bernd@bzed.de>
2019-11-28 11:03:38 +0100

    Also ci-build in fedora

--- .ci-build/build.sh
+++ .ci-build/build.sh
@@ -4,31 +4,29 @@ set -x
 
 export PATH=/usr/sbin:/usr/bin:/sbin:/bin
 
-eval "$(dpkg-buildflags --export=sh)"
-
-export DEB_HOST_MULTIARCH=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
+if lsb_release -d | grep -qi -e debian -e ubuntu; then
+	eval "$(dpkg-buildflags --export=sh)"
+	export DEB_HOST_MULTIARCH=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
+	export PYTHONS="$(pyversions -v -r '>= 2.3'; py3versions -v -r '>= 3.4')"
+	SCONSOPTS="${SCONSOPTS} libdir=/usr/lib/${DEB_HOST_MULTIARCH}"
+else
+	SCONSOPTS="${SCONSOPTS} libdir=/usr/lib"
+fi
 
 SCONSOPTS="${SCONSOPTS} $@ prefix=/usr"
 SCONSOPTS="${SCONSOPTS} systemd=yes"
 SCONSOPTS="${SCONSOPTS} nostrip=yes"
 SCONSOPTS="${SCONSOPTS} dbus_export=yes"
 SCONSOPTS="${SCONSOPTS} docdir=/usr/share/doc/gpsd"
-SCONSOPTS="${SCONSOPTS} libdir=/usr/lib/${DEB_HOST_MULTIARCH}"
 SCONSOPTS="${SCONSOPTS} gpsd_user=gpsd"
 SCONSOPTS="${SCONSOPTS} gpsd_group=dialout"
 SCONSOPTS="${SCONSOPTS} debug=yes"
-
-if dpkg -s qtbase5-dev 1>/dev/null 2>&1; then
-    SCONSOPTS="${SCONSOPTS} qt_versioned=5"
-fi
+SCONSOPTS="${SCONSOPTS} qt_versioned=5"
 
 export SCONSOPTS
 
-
-export PYTHONS="$(pyversions -v -r '>= 2.3'; py3versions -v -r '>= 3.4')"
-
 if [ -z "$PYTHONS" ]; then
-    export PYTHONS="2 3"
+    export PYTHONS="3"
 fi
 
 if [ -n "${SCAN_BUILD}" ]; then
